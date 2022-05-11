5479efccfb05c937ac24f39d0274943e40eb1494
Ponkhy <github@myon.lu>
2022-05-11 14:55:09 +0200

    Added Gitea Template (#11)

--- templates/_list.ts
+++ templates/_list.ts
@@ -1,3 +1,4 @@
 export { default as adminer } from "./adminer";
 export { default as wordpress } from "./wordpress";
+export { default as gitea } from "./gitea";
 export { default as directus } from "./directus";
