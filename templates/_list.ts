293b956264ca3a11ab5bd3d5fee81999beb1b1c1
Ponkhy <github@myon.lu>
2022-05-12 15:03:14 +0200

    Added Statping-ng Template (#15)

--- templates/_list.ts
+++ templates/_list.ts
@@ -4,4 +4,5 @@ export { default as directus } from "./directus";
 export { default as gitea } from "./gitea";
 export { default as heimdall } from "./heimdall";
 export { default as portainer } from "./portainer";
+export { default as statpingNg } from "./statpingNg";
 export { default as wordpress } from "./wordpress";
