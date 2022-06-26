c57a77157209096a09ad89ee9631da4ebefef0e3
Ponkhy <github@myon.lu>
2022-06-26 22:34:53 +0200

    Added MeTube Template (#32)
    * Added MeTube Template
    
    * Renamed downloadPath

---
+++ templates/metube.ts
@@ -0,0 +1,71 @@
+import {
+  createTemplate,
+  Services,
+} from "~templates-utils";
+
+export default createTemplate({
+  name: "MeTube",
+  schema: {
+    type: "object",
+    required: [
+      "projectName",
+      "serviceName",
+      "domain",
+      "downloadPath",
+    ],
+    properties: {
+      projectName: {
+        type: "string",
+        title: "Project Name",
+      },
+      serviceName: {
+        type: "string",
+        title: "App Service Name",
+        default: "metube",
+      },
+      domain: {
+        type: "string",
+        title: "Domain",
+      },
+      downloadPath: {
+        type: "string",
+        title: "Downloads Volume Name",
+        default: "downloads",
+      },
+    },
+  } as const,
+  generate({
+    projectName,
+    serviceName,
+    domain,
+    downloadPath,
+  }) {
+    const services: Services = [];
+
+    services.push({
+      type: "app",
+      data: {
+        projectName,
+        serviceName: serviceName,
+        source: {
+          type: "image",
+          image: "alexta69/metube",
+        },
+        proxy: {
+          port: 8081,
+          secure: true,
+        },
+        domains: [{ name: domain }],
+        volumes: [
+          {
+            type: "volume",
+            source: downloadPath,
+            target: "/downloads",
+          },
+        ],
+      },
+    });
+
+    return { services };
+  },
+});
