572db0cafe9104dfe1f048eb9c9dcf9ca0b7b090
rb rb <bedeoan.raul@gmail.com>
2022-07-12 12:44:40 +0300

    add meta for adminer

--- templates/adminer/index.ts
+++ templates/adminer/index.ts
@@ -5,6 +5,19 @@ import {
 
 export default createTemplate({
   name: "Adminer",
+  meta: {
+    description:
+      "Adminer (formerly phpMinAdmin) is a full-featured database management tool written in PHP. Conversely to phpMyAdmin, it consist of a single file ready to deploy to the target server. Adminer is available for MySQL, MariaDB, PostgreSQL, SQLite, MS SQL, Oracle, Elasticsearch, MongoDB and others via plugin.",
+    changeLog: [{ date: "2022-07-12", description: "first release" }],
+    links: [
+      { label: "Website", url: "https://www.adminer.org/" },
+      { label: "Github", url: "https://github.com/vrana/adminer/" }
+    ],
+    contributors: [
+      { name: "Ponky", url: "https://github.com/Ponkhy" },
+      { name: "Andrei Canta", url: "https://github.com/deiucanta" }
+    ],
+  },
   schema: {
     type: "object",
     required: [
