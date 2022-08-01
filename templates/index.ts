0f8b60ad42e5f4616fd3198c5eddf120d774017b
Ivan <61393936+ivanonpc-22@users.noreply.github.com>
2022-08-01 10:31:08 -0600

    whiteboard template
    add whiteboard template.

--- templates/index.ts
+++ templates/index.ts
@@ -21,3 +21,4 @@ export { default as statpingNg } from "./statpingNg";
 export { default as strapi } from "./strapi";
 export { default as wikijs } from "./wikijs";
 export { default as wordpress } from "./wordpress";
+export { default as whiteboard } from "./whiteboard";
---
+++ templates/whiteboard/index.ts
@@ -0,0 +1,66 @@
+import { createTemplate, Services } from "~templates-utils";
+
+export default createTemplate({
+  name: "whiteboard",
+  meta: {
+    description:
+      "Lightweight collaborative Whiteboard / Sketchboard ",
+    changeLog: [{ date: "2022-07-12", description: "first release" }],
+    links: [
+      { label: "Website",  url: "https://github.com/cracker0dks/whiteboard" },
+      { label: "Documentation", url: "https://github.com/cracker0dks/whiteboard" },
+      { label: "Github", url: "https://github.com/cracker0dks/whiteboard" },
+    ],
+    contributors: [
+      { name: "cracker0dks", url: "https://github.com/cracker0dks" }
+    ],
+  },
+  schema: {
+    type: "object",
+    required: ["projectName", "serviceName", "domain"],
+    properties: {
+      projectName: {
+        type: "string",
+        title: "Project Name",
+      },
+      serviceName: {
+        type: "string",
+        title: "Service Name",
+        default: "whiteboard",
+      },
+      domain: {
+        type: "string",
+        title: "Domain",
+      },
+    },
+  } as const,
+  generate({ projectName, serviceName, domain }) {
+    const services: Services = [];
+
+    services.push({
+      type: "app",
+      data: {
+        projectName,
+        serviceName: serviceName,
+        source: {
+          type: "image",
+          image: "rofl256/whiteboard",
+        },
+        proxy: {
+          port: 8080,
+          secure: true,
+        },
+        domains: [{ name: domain }],
+        volumes: [
+          {
+            type: "volume",
+            source: "data",
+            target: "/app",
+          },
+        ],
+      },
+    });
+
+    return { services };
+  },
+});
