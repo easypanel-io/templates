6c1c174de193692d7f0de45dfa1cabc6747242dd
bede <bedeoan.raul@gmail.com>
2022-08-09 15:05:00 +0300

    create filebrowser template (#84)

--- templates/index.ts
+++ templates/index.ts
@@ -9,28 +9,29 @@ import template_5 from "./domainmod";
 import template_6 from "./drone-runner";
 import template_7 from "./drone";
 import template_8 from "./duplicati";
-import template_9 from "./flame";
-import template_10 from "./ghost";
-import template_11 from "./gitea";
-import template_12 from "./gotify";
-import template_13 from "./heimdall";
-import template_14 from "./imgproxy";
-import template_15 from "./metube";
-import template_16 from "./miniflux";
-import template_17 from "./mysql-backup";
-import template_18 from "./n8n";
-import template_19 from "./nextcloud";
-import template_20 from "./nzbget";
-import template_21 from "./phpmyadmin";
-import template_22 from "./portainer";
-import template_23 from "./postgres-backup";
-import template_24 from "./psitransfer";
-import template_25 from "./statping-ng";
-import template_26 from "./strapi";
-import template_27 from "./uptimekuma";
-import template_28 from "./whiteboard";
-import template_29 from "./wikijs";
-import template_30 from "./wordpress";
+import template_9 from "./filebrowser";
+import template_10 from "./flame";
+import template_11 from "./ghost";
+import template_12 from "./gitea";
+import template_13 from "./gotify";
+import template_14 from "./heimdall";
+import template_15 from "./imgproxy";
+import template_16 from "./metube";
+import template_17 from "./miniflux";
+import template_18 from "./mysql-backup";
+import template_19 from "./n8n";
+import template_20 from "./nextcloud";
+import template_21 from "./nzbget";
+import template_22 from "./phpmyadmin";
+import template_23 from "./portainer";
+import template_24 from "./postgres-backup";
+import template_25 from "./psitransfer";
+import template_26 from "./statping-ng";
+import template_27 from "./strapi";
+import template_28 from "./uptimekuma";
+import template_29 from "./whiteboard";
+import template_30 from "./wikijs";
+import template_31 from "./wordpress";
 
 const templates = [
   { slug: "ackee", template: template_0 },
@@ -42,28 +43,29 @@ const templates = [
   { slug: "drone-runner", template: template_6 },
   { slug: "drone", template: template_7 },
   { slug: "duplicati", template: template_8 },
-  { slug: "flame", template: template_9 },
-  { slug: "ghost", template: template_10 },
-  { slug: "gitea", template: template_11 },
-  { slug: "gotify", template: template_12 },
-  { slug: "heimdall", template: template_13 },
-  { slug: "imgproxy", template: template_14 },
-  { slug: "metube", template: template_15 },
-  { slug: "miniflux", template: template_16 },
-  { slug: "mysql-backup", template: template_17 },
-  { slug: "n8n", template: template_18 },
-  { slug: "nextcloud", template: template_19 },
-  { slug: "nzbget", template: template_20 },
-  { slug: "phpmyadmin", template: template_21 },
-  { slug: "portainer", template: template_22 },
-  { slug: "postgres-backup", template: template_23 },
-  { slug: "psitransfer", template: template_24 },
-  { slug: "statping-ng", template: template_25 },
-  { slug: "strapi", template: template_26 },
-  { slug: "uptimekuma", template: template_27 },
-  { slug: "whiteboard", template: template_28 },
-  { slug: "wikijs", template: template_29 },
-  { slug: "wordpress", template: template_30 },
+  { slug: "filebrowser", template: template_9 },
+  { slug: "flame", template: template_10 },
+  { slug: "ghost", template: template_11 },
+  { slug: "gitea", template: template_12 },
+  { slug: "gotify", template: template_13 },
+  { slug: "heimdall", template: template_14 },
+  { slug: "imgproxy", template: template_15 },
+  { slug: "metube", template: template_16 },
+  { slug: "miniflux", template: template_17 },
+  { slug: "mysql-backup", template: template_18 },
+  { slug: "n8n", template: template_19 },
+  { slug: "nextcloud", template: template_20 },
+  { slug: "nzbget", template: template_21 },
+  { slug: "phpmyadmin", template: template_22 },
+  { slug: "portainer", template: template_23 },
+  { slug: "postgres-backup", template: template_24 },
+  { slug: "psitransfer", template: template_25 },
+  { slug: "statping-ng", template: template_26 },
+  { slug: "strapi", template: template_27 },
+  { slug: "uptimekuma", template: template_28 },
+  { slug: "whiteboard", template: template_29 },
+  { slug: "wikijs", template: template_30 },
+  { slug: "wordpress", template: template_31 },
 ];
 
 export default templates;
