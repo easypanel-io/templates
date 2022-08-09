3fae2e02ec6f352569479f3b84345be2355615bd
Andrei Canta <deiucanta@gmail.com>
2022-08-09 22:55:50 +0300

    Update index.ts

--- templates/registry/index.ts
+++ templates/registry/index.ts
@@ -10,7 +10,7 @@ export default createTemplate({
   meta: {
     description:
       "The toolset to pack, ship, store, and deliver content. Docker Registry is the Open Source Registry implementation for storing and distributing container images using the OCI Distribution Specification. The goal of this project is to provide a simple, secure, and scalable base for building a large scale registry solution or running a simple private registry. It is a core library for many registry operators including Docker Hub, GitHub Container Registry, GitLab Container Registry and DigitalOcean Container Registry, as well as the CNCF Harbor Project, and VMware Harbor Registry.",
-    instructions: `The Registry doesn't have a UI. To interact with the registry you shoult use the Docker CLI.`,
+    instructions: "The Registry doesn't have a UI. To interact with the registry you should use the Docker CLI.",
     changeLog: [{ date: "2022-08-09", description: "first release" }],
     links: [
       { label: "Website", url: "https://hub.docker.com/_/registry" },
