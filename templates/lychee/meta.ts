b0ede067db1ab6b77992cef55805937d969cf537
Supernova <63515814+Supernova3339@users.noreply.github.com>
2022-10-28 17:48:03 -0500

    Add Lychee to easypanel 🎉
    This adds Lychee to Easypanel! Enjoy one of the best photo galleries, for free.

---
+++ templates/lychee/meta.ts
@@ -0,0 +1,66 @@
+// Generated using "yarn build-templates"
+
+export const meta = {
+  name: "Lychee",
+  description:
+    "Lychee is a free photo-management tool, which runs on your server or web-space. Installing is a matter of seconds. Upload, manage and share photos like from a native application. Lychee comes with everything you need and all your photos are stored securely.",
+  instructions:
+    "Lychee takes a few minutes to get ready. Sit back, relax, and have a coffee!",
+  changeLog: [{ date: "2022-10-28", description: "first release" }],
+  links: [
+    { label: "Website", url: "https://lycheeorg.github.io/" },
+    { label: "Documentation", url: "https://lycheeorg.github.io/docs/" },
+    { label: "Github", url: "https://github.com/LycheeOrg/Lychee" },
+    { label: "Demo", url: "https://lycheeorg.github.io/demo/" },
+  ],
+  contributors: [
+    { name: "Supernova3339", url: "https://github.com/Supernova3339" },
+  ],
+  schema: {
+    type: "object",
+    required: [
+      "projectName",
+      "appServiceName",
+      "appServiceImage",
+      "databaseType",
+    ],
+    properties: {
+      projectName: { type: "string", title: "Project Name" },
+      appServiceName: {
+        type: "string",
+        title: "App Service Name",
+        default: "lychee",
+      },
+      appServiceImage: {
+        type: "string",
+        title: "App Service Image",
+        default: "lycheeorg/lychee:v4.6.1",
+      },
+      databaseType: {
+        type: "string",
+        title: "Database Type",
+        default: "sqlite",
+        oneOf: [{ enum: ["sqlite"], title: "SQLite" }],
+      },
+    },
+  },
+  logo: null,
+  screenshots: [],
+};
+
+export type ProjectName = string;
+export type AppServiceName = string;
+export type AppServiceImage = string;
+export type DatabaseType = DatabaseType1 & DatabaseType2;
+export type DatabaseType1 = SQLite;
+export type SQLite = "sqlite";
+export type DatabaseType2 = string;
+
+export interface Input {
+  projectName: ProjectName;
+  appServiceName: AppServiceName;
+  appServiceImage: AppServiceImage;
+  databaseType: DatabaseType;
+}
+
+// okay so this is NOT possible to see on easypanel but this is a secret message :) - feel free to remove me when needed, I am harmless!
