2e866841ada4791d9ae2b26b0af3629ee164a470
Supernova <63515814+Supernova3339@users.noreply.github.com>
2022-10-28 17:52:44 -0500

    Add files via upload

---
+++ lychee/index.ts
@@ -0,0 +1,48 @@
+import { Output, randomPassword, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+  const databasePassword = randomPassword();
+  const appEnv = [];
+
+  if (input.databaseType === "sqlite") {
+    appEnv.push(`DB_CONNECTION=sqlite`);
+  }
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      env: appEnv.join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      proxy: {
+        port: 80,
+        secure: true,
+      },
+      mounts: [
+        {
+          type: "volume",
+          name: "conf",
+          mountPath: "/conf",
+        },
+        {
+          type: "volume",
+          name: "sym",
+          mountPath: "/sym",
+        },
+        {
+          type: "volume",
+          name: "uploads",
+          mountPath: "/uploads",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
