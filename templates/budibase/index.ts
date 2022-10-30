00b6735ff0bc01ca8968cd04daeff586efe90d37
SuperDev <63515814+Supernova3339@users.noreply.github.com>
2022-10-30 11:35:22 -0500

    Add budibase to Easypanel 🎉

---
+++ templates/budibase/index.ts
@@ -0,0 +1,36 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      proxy: {
+        port: 80,
+        secure: true,
+      },
+      mounts: [
+        {
+          type: "volume",
+          name: "data",
+          mountPath: "/data",
+        },
+        {
+          type: "volume",
+          name: "config",
+          mountPath: "/opt/couchdb/data",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
---
+++ templates/budibase/meta.ts
@@ -0,0 +1,47 @@
+// Generated using "yarn build-templates"
+
+export const meta = {
+  name: "Budibase",
+  description:
+    "Budibase is an open source low-code platform, and the easiest way to build internal apps that improve productivity.",
+  instructions:
+    "Budibase takes a few minutes to get ready. Sit back, relax, and have a cup of tea!",
+  changeLog: [{ date: "2022-10-30", description: "first release" }],
+  links: [
+    { label: "Website", url: "https://budibase.com/" },
+    { label: "Documentation", url: "https://docs.budibase.com/docs" },
+    { label: "Github", url: "https://github.com/Budibase/budibase" },
+  ],
+  contributors: [
+    { name: "Supernova3339", url: "https://github.com/Supernova3339" },
+  ],
+  schema: {
+    type: "object",
+    required: ["projectName", "appServiceName", "appServiceImage"],
+    properties: {
+      projectName: { type: "string", title: "Project Name" },
+      appServiceName: {
+        type: "string",
+        title: "App Service Name",
+        default: "budibase",
+      },
+      appServiceImage: {
+        type: "string",
+        title: "App Service Image",
+        default: "budibase/budibase:v2.0.37",
+      },
+    },
+  },
+  logo: null,
+  screenshots: [],
+};
+
+export type ProjectName = string;
+export type AppServiceName = string;
+export type AppServiceImage = string;
+
+export interface Input {
+  projectName: ProjectName;
+  appServiceName: AppServiceName;
+  appServiceImage: AppServiceImage;
+}
