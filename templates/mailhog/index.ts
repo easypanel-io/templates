27f906e0691fe99c9c38564fb02fdcee050c3c64
Rubén Robles <me@d8vjork.com>
2022-10-31 19:49:07 +0100

    add meilhog template

---
+++ templates/mailhog/index.ts
@@ -0,0 +1,30 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      proxy: {
+        port: 8025,
+        secure: true,
+      },
+      ports: [
+        {
+          published: 1025,
+          target: 1025
+        }
+      ],
+    },
+  });
+
+  return { services };
+}
