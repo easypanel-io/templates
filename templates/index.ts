7e38a43c9d25a9b863064961e46e9c26adaf560c
SuperDev <owen8064@gmail.com>
2022-11-29 07:26:22 -0600

    Add Fider to Easypanel 🎉 (#128)

--- templates/index.ts
+++ templates/index.ts
@@ -28,6 +28,8 @@ import { meta as meta_duplicati } from "./duplicati/meta";
 import { generate as generate_duplicati } from "./duplicati";
 import { meta as meta_element } from "./element/meta";
 import { generate as generate_element } from "./element";
+import { meta as meta_fider } from "./fider/meta";
+import { generate as generate_fider } from "./fider";
 import { meta as meta_filebrowser } from "./filebrowser/meta";
 import { generate as generate_filebrowser } from "./filebrowser";
 import { meta as meta_firefox } from "./firefox/meta";
@@ -148,6 +150,7 @@ const templates = [
   { slug: "drone", meta: meta_drone, generate: generate_drone },
   { slug: "duplicati", meta: meta_duplicati, generate: generate_duplicati },
   { slug: "element", meta: meta_element, generate: generate_element },
+  { slug: "fider", meta: meta_fider, generate: generate_fider },
   { slug: "filebrowser", meta: meta_filebrowser, generate: generate_filebrowser },
   { slug: "firefox", meta: meta_firefox, generate: generate_firefox },
   { slug: "flame", meta: meta_flame, generate: generate_flame },
