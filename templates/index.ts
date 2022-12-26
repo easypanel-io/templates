664682d9668d22419a7438ca3300dd8edfb190f0
SuperDev <owen8064@gmail.com>
2022-12-26 23:17:31 +0000

    hi

--- templates/index.ts
+++ templates/index.ts
@@ -136,6 +136,8 @@ import { meta as meta_simpletorrent } from "./simpletorrent/meta";
 import { generate as generate_simpletorrent } from "./simpletorrent";
 import { meta as meta_soketi } from "./soketi/meta";
 import { generate as generate_soketi } from "./soketi";
+import { meta as meta_sonarr } from "./sonarr/meta";
+import { generate as generate_sonarr } from "./sonarr";
 import { meta as meta_statping_ng } from "./statping-ng/meta";
 import { generate as generate_statping_ng } from "./statping-ng";
 import { meta as meta_strapi } from "./strapi/meta";
@@ -228,6 +230,7 @@ const templates = [
   { slug: "rocketchat", meta: meta_rocketchat, generate: generate_rocketchat },
   { slug: "simpletorrent", meta: meta_simpletorrent, generate: generate_simpletorrent },
   { slug: "soketi", meta: meta_soketi, generate: generate_soketi },
+  { slug: "sonarr", meta: meta_sonarr, generate: generate_sonarr },
   { slug: "statping-ng", meta: meta_statping_ng, generate: generate_statping_ng },
   { slug: "strapi", meta: meta_strapi, generate: generate_strapi },
   { slug: "umami", meta: meta_umami, generate: generate_umami },
