7e43ac61806ff1ddb5661787a9a6227c5c5376cc
DrMxrcy <drmxrcy@gmail.com>
2023-01-11 14:28:56 -0500

    Add SuiteCRM To EasyPanel!

---
+++ templates/suitecrm/meta.ts
@@ -0,0 +1,52 @@
+// Generated using "yarn build-templates"
+
+export const meta = {
+  name: "SuiteCRM",
+  description:
+    "SuiteCRM is a completely open source, enterprise-grade Customer Relationship Management (CRM) application.",
+  instructions: null,
+  changeLog: [{ date: "2023-1-12", description: "first release" }],
+  links: [{ label: "Github", url: "https://github.com/salesagility/SuiteCRM" }],
+  contributors: [{ name: "Mxrcy", url: "https://github.com/DrMxrcy" }],
+  schema: {
+    type: "object",
+    required: [
+      "projectName",
+      "appServiceName",
+      "appServiceImage",
+      "databaseServiceName",
+    ],
+    properties: {
+      projectName: { type: "string", title: "Project Name" },
+      appServiceName: {
+        type: "string",
+        title: "App Service Name",
+        default: "suitecrm",
+      },
+      appServiceImage: {
+        type: "string",
+        title: "App Service Image",
+        default: "bitnami/suitecrm:8",
+      },
+      databaseServiceName: {
+        type: "string",
+        title: "Database Service Name",
+        default: "suitecrm-roadmap-db",
+      },
+    },
+  },
+  logo: null,
+  screenshots: ["screenshot.png"],
+};
+
+export type ProjectName = string;
+export type AppServiceName = string;
+export type AppServiceImage = string;
+export type DatabaseServiceName = string;
+
+export interface Input {
+  projectName: ProjectName;
+  appServiceName: AppServiceName;
+  appServiceImage: AppServiceImage;
+  databaseServiceName: DatabaseServiceName;
+}
