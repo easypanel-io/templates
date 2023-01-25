61070d87ff76bf3dcfcad3df0cd766224e059147
SuperDev <owen8064@gmail.com>
2023-01-25 15:38:49 +0000

    add

---
+++ templates/deluge/index.ts
@@ -0,0 +1,36 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      proxy: {
+        port: 8112,
+        secure: true,
+      },
+      ports: [
+        {
+          protocol: "tcp",
+          published: 6881,
+          target: 6881,
+        },
+        {
+          protocol: "udp",
+          published: 6881,
+          target: 6881,
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
