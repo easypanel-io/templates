047f54104a6afb28636bdc9d809da6e8b7c8c16e
SuperDev <owen8064@gmail.com>
2023-01-25 17:29:28 +0000

    add

---
+++ templates/typesense/index.ts
@@ -0,0 +1,37 @@
+import { Output, randomString, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+  const apikey = randomString(32);
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      env: [
+        `TYPESENSE_API_KEY=${apikey}`,
+        `TYPESENSE_DATA_DIR=/data`,
+        `TYPESENSE_API_ADDRESS=0.0.0.0`,
+      ].join("\n"),
+      proxy: {
+        port: 8108,
+        secure: true,
+      },
+      mounts: [
+        {
+          type: "volume",
+          name: "data",
+          mountPath: "/data",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
