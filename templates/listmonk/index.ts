88eee5cca617ee906aa25cae6a10a95b70361f95
Alexandru Serban <alexandruserban@192.168.1.35>
2023-02-03 14:19:19 +0200

    listmonk svg logo

--- templates/listmonk/index.ts
+++ templates/listmonk/index.ts
@@ -1,8 +1,9 @@
-import { Output, Services } from "~templates-utils";
+import { Output, randomPassword, Services } from "~templates-utils";
 import { Input } from "./meta";
 
 export function generate(input: Input): Output {
   const services: Services = [];
+  const randomPasswordPostgres = randomPassword();
 
   services.push({
     type: "app",
@@ -12,10 +13,13 @@ export function generate(input: Input): Output {
       env: [
         `LISTMONK_app__address=0.0.0.0:9000`,
         `LISTMONK_db__host=${input.appServiceName}_${input.databaseServiceName}`,
-        `LISTMONK_db__user=${input.databaseUser}`,
-        `LISTMONK_db__password=${input.databasePassword}`,
+        `LISTMONK_db__user=postgres`,
+        `LISTMONK_db__password=${randomPasswordPostgres}`,
         `LISTMONK_db__port=5432`,
         `LISTMONK_db__database=listmonk`,
+        `LISTMONK_app__admin_username=${input.adminUsername}`,
+        `LISTMONK_app__admin_password=${input.adminPassword}`,
+        
       ].join("\n"),
       source: {
         type: "image",
@@ -44,7 +48,7 @@ export function generate(input: Input): Output {
       projectName: input.projectName,
       serviceName: input.databaseServiceName,
       image: "postgres:14",
-      password: input.databasePassword,
+      password: randomPasswordPostgres,
     },
   });
 
