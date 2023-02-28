f5a41bac829cdb7e252bb63ddcc6077e1c3cef84
SuperDev <owen8064@gmail.com>
2023-02-28 18:32:29 +0000

    add + fix

---
+++ templates/clickhouse/index.ts
@@ -0,0 +1,63 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      proxy: {
+        port: 8123,
+        secure: true,
+      },
+      mounts: [
+        {
+          type: "volume",
+          name: "event-data",
+          mountPath: "/var/lib/clickhouse",
+        },
+        // pay attention to this - this is how to add additional file content ( works for any template )
+        {
+          type: "file",
+          content: [
+            "<yandex>",
+            "<profiles>",
+            "<default>",
+            "<log_queries>0</log_queries>",
+            "<log_query_threads>0</log_query_threads>",
+            "</default>",
+            "</profiles>",
+            "</yandex>",
+            "",
+          ].join("\n"),
+          mountPath: "/etc/clickhouse-server/users.d/logging.xml",
+        },
+        {
+          type: "file",
+          content: [
+            "<yandex>",
+            "<listen_host>0.0.0.0</listen_host>",
+            "",
+            "<logger>",
+            "<level>warning</level>",
+            "<console>true</console>",
+            "</logger>",
+            "",
+            "</yandex>",
+            "",
+          ].join("\n"),
+          mountPath: "/etc/clickhouse-server/config.d/logging.xml",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
