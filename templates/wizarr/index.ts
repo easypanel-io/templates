b50351567533b7e505d431d1b1e73dfc79af20a2
SuperDev <owen8064@gmail.com>
2023-03-02 01:31:00 -0600

    Add Wizarr to Easypanel 🎉  (#249)
    Co-authored-by: Andrei Canta <deiucanta@gmail.com>

---
+++ templates/wizarr/index.ts
@@ -0,0 +1,44 @@
+import {
+  Output,
+  Services
+} from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      env: [
+        `APP_URL=https://${input.domain}`,
+        `TZ=utc`,
+        `DISABLE_BUILTIN_AUTH=false`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      proxy: {
+        port: 5690,
+        secure: true,
+      },
+      mounts: [
+        {
+          type: "volume",
+          name: "data",
+          mountPath: "/data/database",
+        },
+      ],
+      domains: [
+        {
+          name: input.domain,
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
