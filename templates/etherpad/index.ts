d935ffaf252d7f56f6e949316531b12af19d41d8
SuperDev <owen8064@gmail.com>
2023-03-14 22:14:26 +0000

    add

---
+++ templates/etherpad/index.ts
@@ -0,0 +1,62 @@
+import {
+  Output,
+  randomPassword,
+  randomString,
+  Services
+} from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+  const databasePassword = randomPassword();
+  const etherpadApiKey = randomString(32);
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      env: [
+        `DB_TYPE=postgres`,
+        `DB_HOST=${input.projectName}_${input.databaseServiceName}`,
+        `DB_NAME=${input.databaseServiceName}`,
+        `DB_PORT=5432`,
+        `DB_USER=postgres`,
+        `DB_PASS=${databasePassword}`,
+        `ETHERPAD_API_KEY=${etherpadApiKey}`,
+        `ETHERPAD_PORT=9001`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      proxy: {
+        port: 9001,
+        secure: true,
+      },
+      domains: [
+        {
+          name: input.domain,
+        },
+      ],
+      mounts: [
+        {
+          type: "volume",
+          name: "base",
+          mountPath: "/opt",
+        },
+      ],
+    },
+  });
+
+  services.push({
+    type: "postgres",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.databaseServiceName,
+      password: databasePassword,
+    },
+  });
+
+  return { services };
+}
