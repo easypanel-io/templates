1e82573c2de5da65a60661e56d587bfd437e7484
Ponkhy <github@myon.lu>
2023-03-17 15:43:44 +0100

    [Added]: Shiori

---
+++ templates/shiori/index.ts
@@ -0,0 +1,52 @@
+import { Output, randomPassword, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+  const databasePassword = randomPassword();
+
+  services.push({
+    type: "postgres",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.databaseServiceName,
+      password: databasePassword,
+    },
+  });
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      env: [
+        "SHIORI_DIR=/src/shiori",
+        `SHIORI_DBMS=postgresql`,
+        `SHIORI_PG_USER=postgres`,
+        `SHIORI_PG_PASS=${databasePassword}`,
+        `SHIORI_PG_NAME=${input.projectName}`,
+        `SHIORI_PG_HOST=${input.projectName}_${input.databaseServiceName}`,
+        `SHIORI_PG_PORT=5432`,
+      ].join("\n"),
+      mounts: [
+        {
+          type: "volume",
+          name: "data",
+          mountPath: "/src/shiori",
+        },
+      ],
+      proxy: { port: 8080, secure: true },
+      domains: [
+        {
+          name: input.domain,
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
