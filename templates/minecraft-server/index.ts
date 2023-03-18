e633c747f1f2637634ac607e91fbdade08f8e372
Berk Sümbül <mail@berksmbl.com>
2023-03-18 22:17:52 +0300

    Added more ENV values.

--- templates/minecraft-server/index.ts
+++ templates/minecraft-server/index.ts
@@ -1,25 +1,54 @@
-import { Output, Services } from "~templates-utils";
+import { Output, randomString, Services } from "~templates-utils";
 import { Input } from "./meta";
 
 export function generate(input: Input): Output {
   const services: Services = [];
 
-  const appPorts = [{ published: input.serverPort, target: input.serverPort }];
+  const appPorts = [
+    { published: input.serverPort || 25565, target: input.serverPort || 25565 },
+  ];
   const appEnv = [
+    `VERSION=${input.version || ""}`,
+    `TYPE=${input.type || ""}`,
+    `SERVER_NAME=${input.serverName || ""}`,
+    `MOTD=${input.motd || ""}`,
+    `ICON=${input.iconUrl || ""}`,
+    `DIFFICULTY=${input.difficulty || ""}`,
+    `MAX_PLAYERS=${input.maxPlayers || ""}`,
+    `MAX_WORLD_SIZE=${input.maxWorldSize || ""}`,
     `EULA=${input.eula}`,
-    `SERVER_PORT=${input.serverPort || "25565"}`,
+    `ALLOW_NETHER=${input.allowNether}`,
+    `ANNOUNCE_PLAYER_ACHIEVEMENTS=${input.announcePlayerAchievements}`,
+    `ENABLE_COMMAND_BLOCK=${input.enableCommandBlock}`,
+    `HARDCORE=${input.hardcore}`,
+    `SPAWN_ANIMALS=${input.spawnAnimals}`,
+    `SPAWN_MONSTERS=${input.spawnMonsters}`,
+    `SPAWN_NPCS=${input.spawnNpcs}`,
+    `SNOOPER_ENABLED=${input.snooperEnabled}`,
+    `ONLINE_MODE=${input.onlineMode}`,
     `ENABLE_RCON=${input.enableRcon}`,
+    `ENABLE_QUERY=${input.enableQuery}`,
+    `SERVER_PORT=${input.serverPort || 25565}`,
   ];
 
+  if (input.enableQuery) {
+    appEnv.push(`QUERY_PORT=${input.queryPort || 25565}`);
+    appPorts.push({
+      published: input.queryPort || 25565,
+      target: input.queryPort || 25565,
+      protocol: "udp",
+    });
+  }
+
   if (input.enableRcon) {
+    appEnv.push(
+      `RCON_PORT=${input.rconPort || 25575}`,
+      `RCON_PASSWORD=${input.rconPassword || randomString(10)}`
+    );
     appPorts.push({
       published: input.rconPort || 25575,
       target: input.rconPort || 25575,
     });
-    appEnv.push(
-      `RCON_PASSWORD=${input.rconPassword || ""}`,
-      `RCON_PORT=${input.rconPort || "25575"}`
-    );
   }
 
   services.push({
