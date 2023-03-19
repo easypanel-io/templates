3c65ad84668e24e4ad82ebb7ca0773e678572dda
Ponkhy <github@myon.lu>
2023-03-19 19:04:00 +0100

    [Added]: Grav

---
+++ templates/grav/index.ts
@@ -0,0 +1,26 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      source: { type: "image", image: input.appServiceImage },
+      proxy: { port: 80, secure: true },
+      domains: input.domain ? [{ name: input.domain }] : [],
+      mounts: [
+        {
+          type: "volume",
+          name: "config",
+          mountPath: "/config",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
