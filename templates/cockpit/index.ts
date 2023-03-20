c02c82c1507c4ca08e36ffab383038908e68af29
SuperDev <63515814+Supernova3339@users.noreply.github.com>
2023-03-20 13:11:04 +0000

    add

---
+++ templates/cockpit/index.ts
@@ -0,0 +1,49 @@
+import { Output, randomPassword, randomString, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+  const mongoPassword = randomPassword();
+  const databaseSalt = randomString(32);
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      env: [
+        `COCKPIT_SESSION_NAME=cockpit`,
+        `COCKPIT_SALT=${databaseSalt}`,
+        `COCKPIT_DATABASE_SERVER=mongodb://mongo:${mongoPassword}:27017`,
+        `COCKPIT_DATABASE_NAME=${input.projectName}_${input.databaseServiceName}`
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      proxy: {
+        port: 80,
+        secure: true,
+      },
+      mounts: [
+        {
+          type: "volume",
+          name: "html",
+          mountPath: "/var/ww/html"
+        }
+      ]
+    },
+  });
+
+  services.push({
+    type: "mongo",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.databaseServiceName,
+      image: "mongo:4",
+      password: mongoPassword,
+    },
+  });
+
+  return { services };
+}
