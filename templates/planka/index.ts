83fc473d489ccef67f39c31a4aeff9f176a53cd5
SuperDev <63515814+Supernova3339@users.noreply.github.com>
2023-03-21 17:20:30 +0000

    add

---
+++ templates/planka/index.ts
@@ -0,0 +1,51 @@
+import {
+  Output,
+  randomPassword,
+  randomString,
+  Services
+} from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+  const secret = randomString(64);
+  const databasePassword = randomPassword();
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      env: [
+        `TRUST_PROXY=0`,
+        `BASE_URL=https://${input.domain}`,
+        `DATABASE_URL=postgres://postgres:${databasePassword}@${input.projectName}_${input.databaseServiceName}:5432/${input.projectName}?sslmode=disable`,
+        `SECRET_KEY=${secret}`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      proxy: {
+        port: 3000,
+        secure: true,
+      },
+      domains: [
+        {
+          name: input.domain,
+        },
+      ],
+    },
+  });
+
+  services.push({
+    type: "postgres",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.databaseServiceName,
+      password: databasePassword,
+    },
+  });
+
+  return { services };
+}
