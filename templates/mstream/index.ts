188f1afc2dbf2a42495982ddfecb97ea20a3ac8c
SuperDev <63515814+Supernova3339@users.noreply.github.com>
2023-03-23 06:47:55 -0500

    Add Mstream to Easypanel 🎉 #289

---
+++ templates/mstream/index.ts
@@ -0,0 +1,36 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      mounts: [
+        {
+          type: "volume",
+          name: "config",
+          mountPath: "/config",
+        },
+        {
+          type: "volume",
+          name: "music",
+          mountPath: "/music",
+        },
+      ],
+      proxy: {
+        port: 3000,
+        secure: true,
+      },
+    },
+  });
+
+  return { services };
+}
