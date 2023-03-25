cbe0d8adb5b7c13d1f5f061b1920f1943b12d970
Berk Sümbül <mail@berksmbl.com>
2023-03-25 04:13:04 +0300

    Updated image and instructions

--- templates/limesurvey/index.ts
+++ templates/limesurvey/index.ts
@@ -10,49 +10,57 @@ export function generate(input: Input): Output {
     data: {
       projectName: input.projectName,
       serviceName: input.appServiceName,
+      source: { type: "image", image: input.appServiceImage },
+      proxy: { port: 8080, secure: true },
       env: [
+        `DB_TYPE=${input.databaseType === "postgres" ? "pgsql" : "mysql"}`,
         `DB_HOST=${input.projectName}_${input.databaseServiceName}`,
-        `DB_USER=mariadb`,
-        `DB_PASS=${databasePassword}`,
+        `DB_PORT=${input.databaseType === "postgres" ? "5432" : "3306"}`,
         `DB_NAME=${input.projectName}`,
-        `ADMIN_USER=${input.limesurveyUsername}`,
-        `ADMIN_PASS=${input.limesurveyPassword}`,
-        `DEBUG=FALSE`,
+        `DB_USERNAME=${input.databaseType}`,
+        `DB_PASSWORD=${databasePassword}`,
+        `ADMIN_USER=${input.limesurveyUsername || "admin"}`,
+        `ADMIN_PASSWORD=${input.limesurveyPassword || randomPassword()}`,
       ].join("\n"),
-      source: {
-        type: "image",
-        image: input.appServiceImage,
-      },
-      proxy: {
-        port: 80,
-        secure: true,
-      },
       mounts: [
         {
           type: "volume",
-          name: "data",
-          mountPath: "/www/html/upload",
+          name: "upload",
+          mountPath: "/var/www/html/upload",
+        },
+        {
+          type: "volume",
+          name: "assets",
+          mountPath: "/var/www/html/assets",
         },
         {
           type: "volume",
           name: "logs",
-          mountPath: "/www/logs",
+          mountPath: "/var/www/html/application/logs",
         },
         {
           type: "volume",
-          name: "custom-assets",
-          mountPath: "/assets/custom",
+          name: "config",
+          mountPath: "/var/www/html/application/config",
         },
       ],
     },
   });
 
   services.push({
-    type: "mariadb",
+    type: input.databaseType,
     data: {
       projectName: input.projectName,
       serviceName: input.databaseServiceName,
       password: databasePassword,
+      image:
+        input.databaseType === "postgres"
+          ? "postgres:13"
+          : input.databaseType === "mariadb"
+          ? "mariadb:10"
+          : input.databaseType === "mysql"
+          ? "mysql:8"
+          : input.databaseType + ":latest",
     },
   });
 
