aa2cec0e1ef3089d44c69a60fc5ab4dd6c28f459
SuperDev <63515814+Supernova3339@users.noreply.github.com>
2023-03-27 15:36:56 +0000

    add

---
+++ templates/radicale/index.ts
@@ -0,0 +1,32 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      env: "TAKE_FILE_OWNERSHIP=false",
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      proxy: {
+        port: 5232,
+        secure: true,
+      },
+      mounts: [
+        {
+          type: "volume",
+          name: "data",
+          mountPath: "/data",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
