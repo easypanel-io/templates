5bcdaa317650fc78f3edbd128429f51bd0d35bd0
Rubén Robles <me@d8vjork.com>
2023-03-30 20:33:16 +0200

    add muse template (discord music bot)

---
+++ templates/muse/index.ts
@@ -0,0 +1,26 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      env: [
+        `DISCORD_TOKEN=${input.discordToken}`,
+        `YOUTUBE_API_KEY=${input.youtubeApiKey}`,
+        `SPOTIFY_CLIENT_ID=${input.spotifyClientId}`,
+        `SPOTIFY_CLIENT_SECRET=${input.spotifyClientSecret}`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: "codetheweb/muse:2.1"
+      },
+    },
+  });
+
+  return { services };
+}
