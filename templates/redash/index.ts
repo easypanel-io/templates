aee0217185cec9a2c4a17bdac4398e340a9b884a
Thierry BOULOGNE <tboulogne@facile-data.fr>
2023-04-06 18:45:49 +0200

    remove default project name add workers and scheduler

--- templates/redash/index.ts
+++ templates/redash/index.ts
@@ -12,6 +12,9 @@ export function generate(input: Input): Output {
   const cookieSecretkey = randomString(32);
   const randomPasswordRedis = randomPassword();
   const randomPasswordPostgres = randomPassword();
+  const transportValue = input.mailTransport
+    ? `REDASH_MAIL_USE_${input.mailTransport}=true`
+    : "";
 
   services.push({
     type: "app",
@@ -29,9 +32,9 @@ export function generate(input: Input): Output {
         `REDASH_WEB_WORKERS=4`,
         `REDASH_MAIL_SERVER=${input.mailServer}`,
         `REDASH_MAIL_PORT=${input.mailPort}`,
-        `REDASH_MAIL_USE_${input.mailTransport}=true`,
         `REDASH_MAIL_USERNAME=${input.mailUser}`,
         `REDASH_MAIL_PASSWORD=${input.mailPassword}`,
+        `${transportValue}`,
       ].join("\n"),
       mounts: [
         {
@@ -51,6 +54,99 @@ export function generate(input: Input): Output {
       deploy: {
         command:
           "/usr/local/bin/python /app/manage.py database create_tables ; /app/bin/docker-entrypoint server",
+      },
+    },
+  });
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: `${input.projectName}-scheduler`,
+      env: [
+        `PYTHONUNBUFFERED=0`,
+        `REDASH_LOG_LEVEL=INFO`,
+        `REDASH_SECRET_KEY=${secretkey}`,
+        `REDASH_COOKIE_SECRET=${cookieSecretkey}`,
+        `REDASH_REDIS_URL=redis://default:${randomPasswordRedis}@${input.projectName}_${input.redisServiceName}:6379/0`,
+        `QUEUES=celery`,
+        `WORKERS_COUNT=1`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      mounts: [
+        {
+          type: "volume",
+          name: "app-data",
+          mountPath: "/app",
+        },
+      ],
+      deploy: {
+        command: "/app/bin/docker-entrypoint scheduler",
+      },
+    },
+  });
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: `${input.projectName}-scheduled_worker`,
+      env: [
+        `PYTHONUNBUFFERED=0`,
+        `REDASH_LOG_LEVEL=INFO`,
+        `REDASH_SECRET_KEY=${secretkey}`,
+        `REDASH_COOKIE_SECRET=${cookieSecretkey}`,
+        `REDASH_REDIS_URL=redis://default:${randomPasswordRedis}@${input.projectName}_${input.redisServiceName}:6379/0`,
+        `QUEUES=scheduled_queries,schemas`,
+        `WORKERS_COUNT=1`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      mounts: [
+        {
+          type: "volume",
+          name: "app-data",
+          mountPath: "/app",
+        },
+      ],
+      deploy: {
+        command: "/app/bin/docker-entrypoint worker",
+      },
+    },
+  });
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: `${input.projectName}-adhoc_worker`,
+      env: [
+        `PYTHONUNBUFFERED=0`,
+        `REDASH_LOG_LEVEL=INFO`,
+        `REDASH_SECRET_KEY=${secretkey}`,
+        `REDASH_COOKIE_SECRET=${cookieSecretkey}`,
+        `REDASH_REDIS_URL=redis://default:${randomPasswordRedis}@${input.projectName}_${input.redisServiceName}:6379/0`,
+        `QUEUES=queries`,
+        `WORKERS_COUNT=2`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      mounts: [
+        {
+          type: "volume",
+          name: "app-data",
+          mountPath: "/app",
+        },
+      ],
+      deploy: {
+        command: "/app/bin/docker-entrypoint worker",
       },
     },
   });
