aaa1ae2f67d4ccd34920cbaf9a03f29b33f517b1
Berk Sümbül <mail@berksmbl.com>
2023-04-08 13:16:54 +0300

    Fix

--- templates/peertube/index.ts
+++ templates/peertube/index.ts
@@ -10,7 +10,33 @@ export function generate(input: Input): Output {
   const services: Services = [];
   const databasePassword = randomPassword();
   const redisPassword = randomPassword();
-  const peertubeSecret = randomString(32);
+
+  const appEnv = [
+    `PEERTUBE_WEBSERVER_HOSTNAME=${input.domain}`,
+    `PEERTUBE_SECRET=${randomString(32)}`,
+    `PEERTUBE_DB_NAME=${input.projectName}`,
+    `PEERTUBE_DB_HOSTNAME=${input.projectName}_${input.databaseServiceName}`,
+    `PEERTUBE_DB_USERNAME=postgres`,
+    `PEERTUBE_DB_PASSWORD=${databasePassword}`,
+    `PEERTUBE_REDIS_HOSTNAME=${input.projectName}_${input.redisServiceName}`,
+    `PEERTUBE_REDIS_AUTH=${redisPassword}`,
+    `PEERTUBE_ADMIN_EMAIL=${input.peertubeAdminMail}`,
+    `PEERTUBE_SIGNUP_ENABLED=${input.peertubeSignupEnabled}`,
+    `PEERTUBE_TRANSCODING_ENABLED=${input.peertubeTranscodingEnabled}`,
+    `PEERTUBE_CONTACT_FORM_ENABLED=${input.peertubeContactFormEnabled}`,
+  ];
+
+  if (input.peertubeSmtpHostname) {
+    appEnv.push(
+      `PEERTUBE_SMTP_HOSTNAME=${input.peertubeSmtpHostname}`,
+      `PEERTUBE_SMTP_PORT=${input.peertubeSmtpPort || ""}`,
+      `PEERTUBE_SMTP_USERNAME=${input.peertubeSmtpUsername || ""}`,
+      `PEERTUBE_SMTP_PASSWORD=${input.peertubeSmtpPassword || ""}`,
+      `PEERTUBE_SMTP_FROM=${input.peertubeSmtpFrom || ""}`,
+      `PEERTUBE_SMTP_TLS=${input.peertubeSmtpTls}`,
+      `PEERTUBE_SMTP_DISABLE_STARTTLS=${input.peertubeSmtpDisableStarttls}`
+    );
+  }
 
   services.push({
     type: "app",
@@ -19,31 +45,8 @@ export function generate(input: Input): Output {
       serviceName: input.appServiceName,
       source: { type: "image", image: input.appServiceImage },
       proxy: { port: 9000, secure: true },
-      domains:
-        input.peertubeHostname === "localhost"
-          ? []
-          : [{ name: input.peertubeHostname }],
-      env: [
-        `PEERTUBE_WEBSERVER_HOSTNAME=${input.peertubeHostname}`,
-        `PEERTUBE_SECRET=${peertubeSecret}`,
-        `PEERTUBE_DB_NAME=${input.projectName}`,
-        `PEERTUBE_DB_HOSTNAME=${input.projectName}_${input.databaseServiceName}`,
-        `PEERTUBE_DB_USERNAME=postgres`,
-        `PEERTUBE_DB_PASSWORD=${databasePassword}`,
-        `PEERTUBE_REDIS_HOSTNAME=${input.projectName}_${input.redisServiceName}`,
-        `PEERTUBE_REDIS_AUTH=${redisPassword}`,
-        `PEERTUBE_SMTP_HOSTNAME=${input.peertubeSmtpHostname}`,
-        `PEERTUBE_SMTP_PORT=${input.peertubeSmtpPort}`,
-        `PEERTUBE_SMTP_USERNAME=${input.peertubeSmtpUsername || ""}`,
-        `PEERTUBE_SMTP_PASSWORD=${input.peertubeSmtpPassword || ""}`,
-        `PEERTUBE_SMTP_TLS=${input.peertubeSmtpTls}`,
-        `PEERTUBE_SMTP_DISABLE_STARTTLS=${input.peertubeSmtpDisableStarttls}`,
-        `PEERTUBE_SMTP_FROM=${input.peertubeSmtpFrom}`,
-        `PEERTUBE_ADMIN_EMAIL=${input.peertubeAdminMail}`,
-        `PEERTUBE_SIGNUP_ENABLED=${input.peertubeSignupEnabled}`,
-        `PEERTUBE_TRANSCODING_ENABLED=${input.peertubeTranscodingEnabled}`,
-        `PEERTUBE_CONTACT_FORM_ENABLED=${input.peertubeContactFormEnabled}`,
-      ].join("\n"),
+      domains: [{ name: input.domain }],
+      env: appEnv.join("\n"),
       mounts: [
         { type: "volume", name: "assets", mountPath: "/app/client/dist" },
         { type: "volume", name: "data", mountPath: "/data" },
