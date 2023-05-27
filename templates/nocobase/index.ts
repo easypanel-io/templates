82bc28909eaf6defe5c90e9cf2023214dd08aaa4
SuperDev <63515814+Supernova3339@users.noreply.github.com>
2023-05-27 12:12:25 +0000

    Add Nocobase to Easypanel 🎉  (#324)
    * add
    
    * Update templates/nocobase/meta.yaml
    
    Co-authored-by: Ponkhy <github@myon.lu>
    
    ---------
    
    Co-authored-by: Ponkhy <github@myon.lu>

---
+++ templates/nocobase/index.ts
@@ -0,0 +1,53 @@
+import {
+  Output,
+  randomPassword,
+  Services
+} from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+  const databasePassword = randomPassword();
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      env: [
+        `DB_DIALECT=postgres`,
+        `DB_HOST=${input.projectName}_${input.databaseServiceName}`,
+        `DB_DATABASE=${input.projectName}`,
+        `DB_USER=postgres`,
+        `DB_PASSWORD=${databasePassword}`,
+        `LOCAL_STORAGE_BASE_URL=/storage/uploads`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      proxy: {
+        port: 80,
+        secure: true,
+      },
+      mounts: [
+        {
+          type: "volume",
+          name: "uploads",
+          mountPath: "/storage/uploads",
+        },
+      ],
+    },
+  });
+
+  services.push({
+    type: "postgres",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.databaseServiceName,
+      password: databasePassword,
+    },
+  });
+
+  return { services };
+}
