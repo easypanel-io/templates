1b4aa9ffafd6ac7be176d8ca8dae71471ef35b8b
Alexandru Serban <41323656+serban-alexandru@users.noreply.github.com>
2023-06-30 19:21:50 +0300

    qdrant template (#385)
    * qdrant template
    
    * fixes
    
    * fixes
    
    * add volume mount
    
    ---------
    
    Co-authored-by: Andrei Canta <deiucanta@gmail.com>

---
+++ templates/qdrant/index.ts
@@ -0,0 +1,27 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      proxy: {
+        port: 6333,
+        secure: true,
+      },
+      mounts: [
+        { type: "volume", name: "storage", mountPath: "/qdrant/storage" },
+      ],
+    },
+  });
+
+  return { services };
+}
