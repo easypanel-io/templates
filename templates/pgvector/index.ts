9e256c827f2a8028541886cc96438e7e83e3bb3b
Andrei Canta <deiucanta@gmail.com>
2023-06-30 18:34:53 +0200

    fixes

--- templates/pgvector/index.ts
+++ templates/pgvector/index.ts
@@ -5,27 +5,11 @@ export function generate(input: Input): Output {
   const services: Services = [];
 
   services.push({
-    type: "app",
+    type: "postgres",
     data: {
       projectName: input.projectName,
-      serviceName: input.appServiceName,
-      env: [
-        `POSTGRES_PASSWORD=${input.postgresPassword}`,
-        `POSTGRES_HOST_AUTH_METHOD=trust`,
-      ].join("\n"),
-      source: {
-        type: "image",
-        image: input.appServiceImage,
-      },
-      deploy: {
-        replicas: 1,
-        command: "",
-        zeroDowntime: true,
-      },
-      proxy: {
-        port: 80,
-        secure: true,
-      },
+      serviceName: input.serviceName,
+      image: input.serviceImage,
     },
   });
 
