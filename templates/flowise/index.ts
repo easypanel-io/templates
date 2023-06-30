bffe6f112ec8b35aec5e2ef7f47e8258f565cfde
Alexandru Serban <41323656+serban-alexandru@users.noreply.github.com>
2023-06-30 19:46:02 +0300

    LangFlow and Flowise (#383)
    * LangFlow and Flowise
    
    * fixes
    
    ---------
    
    Co-authored-by: Andrei Canta <deiucanta@gmail.com>

---
+++ templates/flowise/index.ts
@@ -0,0 +1,38 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      env: [
+        `FLOWISE_USERNAME=${input.username}`,
+        `FLOWISE_PASSWORD=${input.password}`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      proxy: {
+        port: 80,
+        secure: true,
+      },
+      mounts: [
+        {
+          type: "volume",
+          name: "data",
+          mountPath: "/.root/flowise",
+        },
+      ],
+      deploy: {
+        command: "sleep 3; flowise start",
+      },
+    },
+  });
+
+  return { services };
+}
---
+++ templates/langflow/index.ts
@@ -0,0 +1,45 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      source: {
+        type: "github",
+        owner: "logspace-ai",
+        repo: "langflow",
+        ref: "dev",
+        path: "/docker_example",
+        autoDeploy: false,
+      },
+      build: {
+        type: "dockerfile",
+        file: "Dockerfile",
+      },
+      proxy: {
+        port: 7860,
+        secure: true,
+      },
+      basicAuth: [
+        {
+          username: input.username,
+          password: input.password,
+        },
+      ],
+      mounts: [
+        {
+          type: "volume",
+          name: "data",
+          mountPath: "/home/user/app",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
