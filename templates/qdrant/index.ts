59d69dd8a4659bd0b97d197a901bb1cdab0b9d14
serban-alexandru <alexserb2009@yahoo.com>
2023-06-30 10:17:10 +0300

    qdrant template

---
+++ templates/qdrant/index.ts
@@ -0,0 +1,29 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      deploy: {
+        replicas: 1,
+        command: null,
+        zeroDowntime: true,
+      },
+      proxy: {
+        port: 80,
+        secure: true,
+      },
+    },
+  });
+
+  return { services };
+}
