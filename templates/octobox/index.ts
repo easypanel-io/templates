c32bfe684536874a870ea92b9a12612d4b804025
SuperDev <63515814+Supernova3339@users.noreply.github.com>
2023-06-30 23:38:03 -0500

    Update index.ts

--- templates/octobox/index.ts
+++ templates/octobox/index.ts
@@ -24,6 +24,7 @@ export function generate(input: Input): Output {
         `OCTOBOX_DATABASE_PASSWORD=${databasePassword}`,
         `OCTOBOX_DATABASE_HOST=${input.projectName}_${input.databaseServiceName}`,
         `REDIS_URL=redis://default:${redisPassword}@${input.projectName}_${input.redisServiceName}:6379`,
+        `ALLOW_ALL_HOSTNAMES=true`,
       ].join("\n"),
       source: {
         type: "image",
