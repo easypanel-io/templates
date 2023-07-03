1cd6eea28416bec25d2067bc73c067ea04a1ec21
Berk Sümbül <berksmbl@gmail.com>
2023-07-03 03:50:12 +0300

    Update index.ts

--- templates/ghost/index.ts
+++ templates/ghost/index.ts
@@ -18,7 +18,7 @@ export function generate(input: Input): Output {
         `url=https://${input.domain}`,
         `database__client=mysql`,
         `database__connection__host=${input.projectName}_${input.databaseServiceName}`,
-        `database__connection__user=${input.projectName}`,
+        `database__connection__user=${input.databaseType}`,
         `database__connection__password=${databasePassword}`,
         `database__connection__database=${input.projectName}`,
       ].join("\n"),
@@ -37,7 +37,7 @@ export function generate(input: Input): Output {
   });
 
   services.push({
-    type: "mysql",
+    type: input.databaseType,
     data: {
       projectName: input.projectName,
       serviceName: input.databaseServiceName,
