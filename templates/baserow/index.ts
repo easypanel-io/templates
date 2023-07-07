d78d5f45043a1d987eda7e4a9031c4759376a460
Berk Sümbül <mail@berksmbl.com>
2023-07-07 15:55:42 +0300

    external redis and database support

--- templates/baserow/index.ts
+++ templates/baserow/index.ts
@@ -1,24 +1,43 @@
-import { Output, Services } from "~templates-utils";
+import { Output, Services, randomPassword } from "~templates-utils";
 import { Input } from "./meta";
 
 export function generate(input: Input): Output {
   const services: Services = [];
+  const databasePassword = randomPassword();
+  const redisPassword = randomPassword();
 
   services.push({
     type: "app",
     data: {
       projectName: input.projectName,
       serviceName: input.appServiceName,
-      source: {
-        type: "image",
-        image: input.appServiceImage,
-      },
-      env: `BASEROW_PUBLIC_URL=https://${input.domain}`,
-      proxy: {
-        port: 80,
-        secure: true,
-      },
+      source: { type: "image", image: input.appServiceImage },
+      domains: input.domain ? [{ name: input.domain }] : [],
+      proxy: { port: 80, secure: true },
       mounts: [{ type: "volume", name: "data", mountPath: "/baserow/data" }],
+      env: [
+        `BASEROW_PUBLIC_URL=https://${input.domain}`,
+        `DATABASE_URL=postgres://postgres:${databasePassword}@${input.projectName}_${input.databaseServiceName}:5432/${input.projectName}`,
+        `REDIS_URL=redis://default:${redisPassword}@${input.projectName}_${input.redisServiceName}:6379`,
+      ].join("\n"),
+    },
+  });
+
+  services.push({
+    type: "postgres",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.databaseServiceName,
+      password: databasePassword,
+    },
+  });
+
+  services.push({
+    type: "redis",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.redisServiceName,
+      password: redisPassword,
     },
   });
 
