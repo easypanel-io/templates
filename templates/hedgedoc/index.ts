408bfe49bb00dce1e65cedd543380faf0e812759
Berk Sümbül <mail@berksmbl.com>
2023-07-11 16:30:06 +0300

    update: HedgeDoc
    Update to v1.9.8 and return to main image

--- templates/hedgedoc/index.ts
+++ templates/hedgedoc/index.ts
@@ -1,25 +1,38 @@
-import { Output, Services } from "~templates-utils";
+import { Output, Services, randomPassword } from "~templates-utils";
 import { Input } from "./meta";
 
 export function generate(input: Input): Output {
   const services: Services = [];
+  const databasePassword = randomPassword();
 
   services.push({
     type: "app",
     data: {
       projectName: input.projectName,
       serviceName: input.appServiceName,
-      env: [`CMD_DOMAIN=$(PRIMARY_DOMAIN)`].join("\n"),
-      source: {
-        type: "image",
-        image: input.appServiceImage,
-      },
-      domains: [
+      source: { type: "image", image: input.appServiceImage },
+      domains: [{ host: "$(EASYPANEL_DOMAIN)", port: 3000 }],
+      mounts: [
         {
-          host: "$(EASYPANEL_DOMAIN)",
-          port: 80,
+          type: "volume",
+          name: "uploads",
+          mountPath: "/hedgedoc/public/uploads",
         },
       ],
+      env: [
+        `CMD_DOMAIN=$(PRIMARY_DOMAIN)`,
+        `CMD_PROTOCOL_USESSL=true`,
+        `CMD_DB_URL=postgres://postgres:${databasePassword}@$(PROJECT_NAME)_${input.databaseServiceName}:5432/$(PROJECT_NAME)`,
+      ].join("\n"),
+    },
+  });
+
+  services.push({
+    type: "postgres",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.databaseServiceName,
+      password: databasePassword,
     },
   });
 
