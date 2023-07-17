190e13c2e375ba298550ae0e41bd15cb2c05cebc
Berk Sümbül <mail@berksmbl.com>
2023-07-17 11:22:43 +0300

    update: Botpress

--- templates/botpress/index.ts
+++ templates/botpress/index.ts
@@ -4,39 +4,25 @@ import { Input } from "./meta";
 export function generate(input: Input): Output {
   const services: Services = [];
   const databasePassword = randomPassword();
+  const redisPassword = randomPassword();
 
   services.push({
     type: "app",
     data: {
       projectName: input.projectName,
       serviceName: input.appServiceName,
+      source: { type: "image", image: input.appServiceImage },
+      domains: [{ host: "$(EASYPANEL_DOMAIN)", port: 3001 }],
+      mounts: [{ type: "volume", name: "data", mountPath: "/botpress/data" }],
       env: [
+        `PORT=3000`,
         `BP_HOST=0.0.0.0`,
+        `BP_PRODUCTION=true`,
         `NODE_ENV=production`,
-        `PG_HOST=$(PROJECT_NAME)_${input.databaseServiceName}`,
-        `PG_PORT=5432`,
-        `PG_USER=postgres`,
-        `PG_PASSWORD=${databasePassword}`,
-        `PG_SSL=false`,
-        `PORT=80`,
+        `EXTERNAL_URL=https://$(PRIMARY_DOMAIN)`,
+        `REDIS_URL=redis://default:${redisPassword}@$(PROJECT_NAME)_${input.redisServiceName}:6379`,
+        `DATABASE_URL=postgres://postgres:${databasePassword}@$(PROJECT_NAME)_${input.databaseServiceName}:5432/$(PROJECT_NAME)`,
       ].join("\n"),
-      source: {
-        type: "image",
-        image: input.appServiceImage,
-      },
-      domains: [
-        {
-          host: "$(EASYPANEL_DOMAIN)",
-          port: 80,
-        },
-      ],
-      mounts: [
-        {
-          type: "volume",
-          name: "data",
-          mountPath: "/botpress/data",
-        },
-      ],
     },
   });
 
@@ -46,6 +32,15 @@ export function generate(input: Input): Output {
       projectName: input.projectName,
       serviceName: input.databaseServiceName,
       password: databasePassword,
+    },
+  });
+
+  services.push({
+    type: "redis",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.redisServiceName,
+      password: redisPassword,
     },
   });
 
