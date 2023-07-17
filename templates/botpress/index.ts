6833ff89801801f1d845cca6e46c34a88f871860
Berk Sümbül <mail@berksmbl.com>
2023-07-17 13:49:45 +0300

    Add Lang server option

--- templates/botpress/index.ts
+++ templates/botpress/index.ts
@@ -5,6 +5,36 @@ export function generate(input: Input): Output {
   const services: Services = [];
   const databasePassword = randomPassword();
 
+  const appEnv = [
+    `DATABASE_URL=postgres://postgres:${databasePassword}@$(PROJECT_NAME)_${input.databaseServiceName}:5432/$(PROJECT_NAME)`,
+    `BP_PRODUCTION=true`,
+    `BPFS_STORAGE=database`,
+    `EXTERNAL_URL=https://$(PRIMARY_DOMAIN)`,
+  ];
+
+  if (input.enableLangServer) {
+    appEnv.push(
+      `BP_MODULE_NLU_LANGUAGESOURCES='[{ "endpoint": "http://$(PROJECT_NAME)_$(SERVICE_NAME)-lang:3100" }]'`
+    );
+    services.push({
+      type: "app",
+      data: {
+        projectName: input.projectName,
+        serviceName: input.appServiceName + "-lang",
+        source: { type: "image", image: input.appServiceImage },
+        domains: [{ host: "$(EASYPANEL_DOMAIN)", port: 3100 }],
+        deploy: { command: "./bp lang --langDir /botpress/data/embeddings" },
+        mounts: [
+          {
+            type: "bind",
+            hostPath: `/etc/easypanel/projects/$(PROJECT_NAME)/${input.appServiceName}/volumes/data`,
+            mountPath: "/botpress/data",
+          },
+        ],
+      },
+    });
+  }
+
   services.push({
     type: "app",
     data: {
@@ -12,30 +42,9 @@ export function generate(input: Input): Output {
       serviceName: input.appServiceName,
       source: { type: "image", image: input.appServiceImage },
       domains: [{ host: "$(EASYPANEL_DOMAIN)", port: 3000 }],
+      deploy: { command: "./bp & ./duckling -p 8000" },
       mounts: [{ type: "volume", name: "data", mountPath: "/botpress/data" }],
-      deploy: { command: "sleep 10 & ./bp" },
-      env: [
-        `DATABASE_URL=postgres://postgres:${databasePassword}@$(PROJECT_NAME)_${input.databaseServiceName}:5432/$(PROJECT_NAME)`,
-        `BP_MODULE_NLU_DUCKLINGURL=http://$(PROJECT_NAME)_$(SERVICE_NAME)-lang:8000`,
-        `BP_MODULE_NLU_LANGUAGESOURCES='[{ "endpoint": "http://$(PROJECT_NAME)_$(SERVICE_NAME)-lang:3100" }]'`,
-        `BP_PRODUCTION=true`,
-        `BPFS_STORAGE=database`,
-        `EXTERNAL_URL=https://$(PRIMARY_DOMAIN)`,
-      ].join("\n"),
-    },
-  });
-
-  services.push({
-    type: "app",
-    data: {
-      projectName: input.projectName,
-      serviceName: input.appServiceName + "-lang",
-      source: { type: "image", image: input.appServiceImage },
-      mounts: [{ type: "volume", name: "data", mountPath: "/botpress/data" }],
-      deploy: {
-        command:
-          "./duckling -p 8000 & ./bp lang --langDir /botpress/data/embeddings --port 3100",
-      },
+      env: appEnv.join("\n"),
     },
   });
 
