0a49b14c2c12859727b641893446081bb84d0822
Matheus Breguêz <matbrgz@gmail.com>
2023-09-21 12:28:55 -0300

    add: Komiser

---
+++ templates/komiser/index.ts
@@ -0,0 +1,45 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+  const sshPort = input.sshPort;
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 3000,
+        },
+      ],
+      mounts: [
+        {
+          type: "volume",
+          name: "data",
+          mountPath: "/data",
+        },
+        {
+          type: "volume",
+          name: "backup",
+          mountPath: "/backup",
+        },
+      ],
+      ports: [
+        {
+          published: sshPort,
+          target: 22,
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
