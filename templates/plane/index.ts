f249f5c4dd4acc48158434673e0f8b5e0cc83a35
Matheus Breguêz <matbrgz@gmail.com>
2023-09-22 01:46:05 -0300

    wip: progress

--- templates/plane/index.ts
+++ templates/plane/index.ts
@@ -10,15 +10,14 @@ export function generate(input: Input): Output {
   const services: Services = [];
   const secretkey = randomString(32);
   const randomPasswordRedis = randomPassword();
-  const randomPasswordPostgres = randomPassword();
+  const databasePassword = randomPassword();
   const minioPassword = randomPassword();
-
 
   services.push({
     type: "app",
     data: {
       projectName: input.projectName,
-      serviceName: input.appServiceName,
+      serviceName: input.appFrontendServiceName,
       env: [
         `NODE_ENV=production`,
         `NEXT_PUBLIC_EXTRA_IMAGE_DOMAINS=`,
@@ -33,14 +32,8 @@ export function generate(input: Input): Output {
         `NEXT_PUBLIC_SLACK_CLIENT_ID=""`,
         `DEBUG=0`,
         `SENTRY_DSN=""`,
-        `PGUSER=postgres`,
-        `PGPASSWORD=${randomPasswordPostgres}`,
-        `PGHOST=$(PROJECT_NAME)_${input.databaseServiceName}`,
-        `PGDATABASE=$(PROJECT_NAME)`,
-        `DATABASE_URL=postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`,
-        `REDIS_HOST="plane-redis"`,
-        `REDIS_PORT="6379"`,
-        `REDIS_URL="redis://${REDIS_HOST}:${REDIS_PORT}/"`,
+        `DATABASE_URL=postgres://postgres:${databasePassword}@$(PROJECT_NAME)_${input.databaseServiceName}:5432/$(PROJECT_NAME)`,
+        `REDIS_URL="redis://${input.databaseServiceName}:6379/"`,
         `EMAIL_HOST=""`,
         `EMAIL_HOST_USER=""`,
         `EMAIL_HOST_PASSWORD=""`,
@@ -61,11 +54,11 @@ export function generate(input: Input): Output {
         `ENABLE_SIGNUP="1"`,
         `NEXT_PUBLIC_API_BASE_URL=https://$(PRIMARY_DOMAIN)`,
         `SECRET_KEY=${secretkey}`,
-        `WEB_URL=https://$(PRIMARY_DOMAIN)`
+        `WEB_URL=https://$(PRIMARY_DOMAIN)`,
       ].join("\n"),
       source: {
         type: "image",
-        image: input.appServiceImage,
+        image: input.appFrontendServiceName,
       },
       domains: [
         {
@@ -73,10 +66,142 @@ export function generate(input: Input): Output {
           port: 3000,
         },
       ],
-      deploy: {
-        command:
-          "bundle exec rails db:chatwoot_prepare && bundle exec rails s -p 3000 -b 0.0.0.0",
+      mounts: [
+        {
+          type: "volume",
+          name: "data",
+          mountPath: "/data/storage",
+        },
+        {
+          type: "volume",
+          name: "app",
+          mountPath: "/app/storage",
+        },
+      ],
+    },
+  });
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appBackendServiceName,
+      env: [
+        `NODE_ENV=production`,
+        `NEXT_PUBLIC_EXTRA_IMAGE_DOMAINS=`,
+        `NEXT_PUBLIC_GOOGLE_CLIENTID=""`,
+        `NEXT_PUBLIC_GITHUB_ID=""`,
+        `NEXT_PUBLIC_GITHUB_APP_NAME=""`,
+        `NEXT_PUBLIC_SENTRY_DSN=""`,
+        `NEXT_PUBLIC_ENABLE_OAUTH=0`,
+        `NEXT_PUBLIC_ENABLE_SENTRY=0`,
+        `NEXT_PUBLIC_ENABLE_SESSION_RECORDER=0`,
+        `NEXT_PUBLIC_TRACK_EVENTS=0`,
+        `NEXT_PUBLIC_SLACK_CLIENT_ID=""`,
+        `DEBUG=0`,
+        `SENTRY_DSN=""`,
+        `DATABASE_URL=postgres://postgres:${databasePassword}@$(PROJECT_NAME)_${input.databaseServiceName}:5432/$(PROJECT_NAME)`,
+        `REDIS_URL="redis://${input.databaseServiceName}:6379/"`,
+        `EMAIL_HOST=""`,
+        `EMAIL_HOST_USER=""`,
+        `EMAIL_HOST_PASSWORD=""`,
+        `EMAIL_PORT=587`,
+        `EMAIL_FROM="Team Plane <team@mailer.plane.so>"`,
+        `EMAIL_USE_TLS="1"`,
+        `EMAIL_USE_SSL="0"`,
+        `AWS_S3_BUCKET_NAME="uploads"`,
+        `FILE_SIZE_LIMIT=5242880`,
+        `OPENAI_API_KEY=""`,
+        `GPT_ENGINE=""`,
+        `GITHUB_CLIENT_SECRET=`,
+        `DOCKERIZED=1`,
+        `USE_MINIO=1`,
+        `NGINX_PORT=80`,
+        `DEFAULT_EMAIL="captain@plane.so"`,
+        `DEFAULT_PASSWORD="password123"`,
+        `ENABLE_SIGNUP="1"`,
+        `NEXT_PUBLIC_API_BASE_URL=https://$(PRIMARY_DOMAIN)`,
+        `SECRET_KEY=${secretkey}`,
+        `WEB_URL=https://$(PRIMARY_DOMAIN)`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appBackendServiceName,
       },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 3000,
+        },
+      ],
+      mounts: [
+        {
+          type: "volume",
+          name: "data",
+          mountPath: "/data/storage",
+        },
+        {
+          type: "volume",
+          name: "app",
+          mountPath: "/app/storage",
+        },
+      ],
+    },
+  });
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appWorkerServiceName,
+      env: [
+        `NODE_ENV=production`,
+        `NEXT_PUBLIC_EXTRA_IMAGE_DOMAINS=`,
+        `NEXT_PUBLIC_GOOGLE_CLIENTID=""`,
+        `NEXT_PUBLIC_GITHUB_ID=""`,
+        `NEXT_PUBLIC_GITHUB_APP_NAME=""`,
+        `NEXT_PUBLIC_SENTRY_DSN=""`,
+        `NEXT_PUBLIC_ENABLE_OAUTH=0`,
+        `NEXT_PUBLIC_ENABLE_SENTRY=0`,
+        `NEXT_PUBLIC_ENABLE_SESSION_RECORDER=0`,
+        `NEXT_PUBLIC_TRACK_EVENTS=0`,
+        `NEXT_PUBLIC_SLACK_CLIENT_ID=""`,
+        `DEBUG=0`,
+        `SENTRY_DSN=""`,
+        `DATABASE_URL=postgres://postgres:${databasePassword}@$(PROJECT_NAME)_${input.databaseServiceName}:5432/$(PROJECT_NAME)`,
+        `REDIS_URL="redis://${input.databaseServiceName}:6379/"`,
+        `EMAIL_HOST=""`,
+        `EMAIL_HOST_USER=""`,
+        `EMAIL_HOST_PASSWORD=""`,
+        `EMAIL_PORT=587`,
+        `EMAIL_FROM="Team Plane <team@mailer.plane.so>"`,
+        `EMAIL_USE_TLS="1"`,
+        `EMAIL_USE_SSL="0"`,
+        `AWS_S3_BUCKET_NAME="uploads"`,
+        `FILE_SIZE_LIMIT=5242880`,
+        `OPENAI_API_KEY=""`,
+        `GPT_ENGINE=""`,
+        `GITHUB_CLIENT_SECRET=`,
+        `DOCKERIZED=1`,
+        `USE_MINIO=1`,
+        `NGINX_PORT=80`,
+        `DEFAULT_EMAIL="captain@plane.so"`,
+        `DEFAULT_PASSWORD="password123"`,
+        `ENABLE_SIGNUP="1"`,
+        `NEXT_PUBLIC_API_BASE_URL=https://$(PRIMARY_DOMAIN)`,
+        `SECRET_KEY=${secretkey}`,
+        `WEB_URL=https://$(PRIMARY_DOMAIN)`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appWorkerServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 3000,
+        },
+      ],
       mounts: [
         {
           type: "volume",
