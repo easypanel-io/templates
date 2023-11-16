c1d2255ad06b0b6e0d74706e876efd0fa544afb2
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2023-11-16 09:16:30 +0000

    Add Email configuration options to the Ever Teams main.yaml

--- templates/ever-teams/index.ts
+++ templates/ever-teams/index.ts
@@ -13,6 +13,14 @@ export function generate(input: Input): Output {
         type: "image",
         image: input.appServiceImage,
       },
+      env: [
+        `SMTP_FROM_ADDRESS=${input.smtpFromAddress}`,
+        `SMTP_HOST=${input.smtpHost}`,
+        `SMTP_PORT=${input.smtpPort}`,
+        `SMTP_SECURE=${input.smtpSecure}`,
+        `SMTP_USERNAME=${input.smtpUsername}`,
+        `SMTP_PASSWORD=${input.smtpPassword}`,
+      ].join("\n"),
       domains: [
         {
           host: "$(EASYPANEL_DOMAIN)",
