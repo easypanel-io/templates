6f584bf9640ad04e6fcf0293f8a75af644483ac0
Mark Topper Diderichsen <mark@ulties.com>
2023-11-30 12:37:24 +0100

    Prettify

--- templates/moodle/index.ts
+++ templates/moodle/index.ts
@@ -1,8 +1,4 @@
-import {
-  Output,
-  randomPassword,
-  Services,
-} from "~templates-utils";
+import { Output, randomPassword, Services } from "~templates-utils";
 import { Input } from "./meta";
 
 export function generate(input: Input): Output {
@@ -10,57 +6,57 @@ export function generate(input: Input): Output {
   const databasePassword = randomPassword();
 
   services.push({
-    type: 'app',
+    type: "app",
     data: {
       projectName: input.projectName,
-      serviceName: 'moodle',
+      serviceName: "moodle",
       source: {
-        'type': 'github',
-        'owner': 'ulties',
-        'repo': 'moodle-docker',
-        'ref': 'main',
-        'path': '/',
-        'autoDeploy': false,
+        type: "github",
+        owner: "ulties",
+        repo: "moodle-docker",
+        ref: "main",
+        path: "/",
+        autoDeploy: false,
       },
       env: [
-        'MOODLE_URL=https://' + input.moodleDomain,
-        'MOODLE_SSLPROXY=true',
-        'MOODLE_DB_TYPE=mysqli',
-        'MOODLE_DB_HOST=' + input.projectName + '_mysql',
-        'MOODLE_DB_NAME=' + input.projectName,
-        'MOODLE_DB_USER=mysql',
-        'MOODLE_DB_PASSWORD=' + databasePassword,
-        'MOODLE_DB_PREFIX=mdl_',
-      ].join('\n'),
-      'deploy': {
-        'replicas': 1,
-        'command': null,
-        'zeroDowntime': true,
+        "MOODLE_URL=https://" + input.moodleDomain,
+        "MOODLE_SSLPROXY=true",
+        "MOODLE_DB_TYPE=mysqli",
+        "MOODLE_DB_HOST=" + input.projectName + "_mysql",
+        "MOODLE_DB_NAME=" + input.projectName,
+        "MOODLE_DB_USER=mysql",
+        "MOODLE_DB_PASSWORD=" + databasePassword,
+        "MOODLE_DB_PREFIX=mdl_",
+      ].join("\n"),
+      deploy: {
+        replicas: 1,
+        command: null,
+        zeroDowntime: true,
       },
-      'domains': [
+      domains: [
         {
-          'host': input.moodleDomain,
-          'https': true,
-          'port': 80,
-          'path': '/',
+          host: input.moodleDomain,
+          https: true,
+          port: 80,
+          path: "/",
         },
       ],
-      'mounts': [
+      mounts: [
         {
-          'type': 'volume',
-          'name': 'moodle_data',
-          'mountPath': '/var/www/moodledata',
+          type: "volume",
+          name: "moodle_data",
+          mountPath: "/var/www/moodledata",
         },
       ],
     },
   });
 
   services.push({
-    type: 'mysql',
+    type: "mysql",
     data: {
       projectName: input.projectName,
-      serviceName: 'mysql',
-      image: 'mysql:8',
+      serviceName: "mysql",
+      image: "mysql:8",
       password: databasePassword,
     },
   });
