120967f98baa9ae36fc8cc062b8d8ec928aa53bc
serban-alexandru <alexserb2009@yahoo.com>
2024-01-24 16:37:33 +0200

    cloud commander template

---
+++ templates/cloudcommander/index.ts
@@ -0,0 +1,44 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      env: [
+        `CLOUDCMD_ROOT=/mnt/fs`,
+        `CLOUDCMD_AUTH=true`,
+        `CLOUDCMD_USERNAME=${input.username}`,
+        `CLOUDCMD_PASSWORD=${input.password}`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 80,
+        },
+      ],
+      mounts: [
+        {
+          type: "bind",
+          hostPath: "/root",
+          mountPath: "/root",
+        },
+        {
+          type: "bind",
+          hostPath: "/",
+          mountPath: "/mnt/fs",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
