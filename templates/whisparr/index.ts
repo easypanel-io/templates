4d02dfe90521c684222a29bfdcdd1b958c1a2fe4
Mark Topper Diderichsen <mark@ulties.com>
2024-03-18 20:11:33 +0100

    Add Whisparr Template

---
+++ templates/whisparr/index.ts
@@ -0,0 +1,33 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 80,
+        },
+      ],
+      mounts: [
+        {
+          type: "volume",
+          name: "config",
+          mountPath: "/config",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
