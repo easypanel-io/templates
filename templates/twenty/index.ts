07c3c0e442cb45427dfe4af43145371674e1c9dc
serban-alexandru <alexserb2009@yahoo.com>
2024-07-26 17:21:31 +0300

    supabase, twenty, dify and plane templates

---
+++ templates/twenty/index.ts
@@ -0,0 +1,30 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "compose",
+    data: {
+      projectName: input.projectName,
+      serviceName: input.appServiceName,
+      source: {
+        type: "git",
+        repo: "git@github.com:easypanel-io/compose.git",
+        ref: "main",
+        rootPath: "/twenty/code",
+        composeFile: "docker-compose.yml",
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 3000,
+          service: "server",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
