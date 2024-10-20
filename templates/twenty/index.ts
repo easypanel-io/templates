5149603f9c40f00aba5c7083c0af70ea2b03b416
ahsonshaikh <ahson.shaikh@arpatech.com>
2024-10-20 15:22:47 +0500

    Replacing the compose service template with App service template

--- templates/twenty/index.ts
+++ templates/twenty/index.ts
@@ -1,27 +1,54 @@
-import { Output, Services } from "~templates-utils";
+import {
+  Output,
+  randomPassword,
+  randomString,
+  Services,
+} from "~templates-utils";
 import { Input } from "./meta";
 
 export function generate(input: Input): Output {
   const services: Services = [];
+  const databasePassword = randomPassword();
+  const accessTokenSecret = randomString(32);
+  const loginTokenSceret = randomString(32);
+  const refreshTokenSecret = randomString(32);
+  const fileTokenSecret = randomString(32);
 
   services.push({
-    type: "compose",
+    type: "app",
     data: {
-      serviceName: input.serviceName,
+      serviceName: input.appServiceName,
+      env: [
+        `PG_DATABASE_URL=postgres://postgres:${databasePassword}@$(PROJECT_NAME)_${input.databaseServiceName}:5432/$(PROJECT_NAME)`,
+        `ACCESS_TOKEN_SECRET=${accessTokenSecret}`,
+        `LOGIN_TOKEN_SECRET=${loginTokenSceret}`,
+        `REFRESH_TOKEN_SECRET=${refreshTokenSecret}`,
+        `FILE_TOKEN_SECRET=${fileTokenSecret}`,
+        `SERVER_URL=https://$(EASYPANEL_DOMAIN)`,
+        "FRONT_BASE_URL=http://localhost:3001",
+        `SIGN_IN_PREFILLED=true`,
+        `STORAGE_TYPE=local`,
+        `ENABLE_DB_MIGRATIONS=true`,
+      ].join("\n"),
       source: {
-        type: "git",
-        repo: "https://github.com/easypanel-io/compose.git",
-        ref: "main",
-        rootPath: "/twenty/code",
-        composeFile: "docker-compose.yml",
+        type: "image",
+        image: input.appServiceImage,
       },
       domains: [
         {
           host: "$(EASYPANEL_DOMAIN)",
           port: 3000,
-          service: "server",
         },
       ],
+    },
+  });
+
+  services.push({
+    type: "postgres",
+    data: {
+      serviceName: input.databaseServiceName,
+      image: "twentycrm/twenty-postgres:v0.10",
+      password: databasePassword,
     },
   });
 
