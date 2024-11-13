6722a0dbcb5af4554f7a8b708dd734764a05d13e
ahsonshaikh <ahson.shaikh@arpatech.com>
2024-11-13 10:31:29 +0500

    Template Added SitrlingPDF

---
+++ templates/stirlingpdf/index.ts
@@ -0,0 +1,30 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      env: [
+        `DOCKER_ENABLE_SECURITY=false`,
+        `INSTALL_BOOK_AND_ADVANCED_HTML_OPS=false`,
+        `LANGS=en_GB`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 8080,
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
