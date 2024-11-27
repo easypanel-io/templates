3c1d57a769571bfcb4daa6bffe42422ac1f032c3
serban-alexandru <alexserb2009@yahoo.com>
2024-11-27 10:19:27 +0200

    hackmd template

---
+++ templates/hackmdio/index.ts
@@ -0,0 +1,50 @@
+import { Output, randomPassword, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+  const databasePassword = randomPassword();
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      env: [
+        `CMD_DB_URL=postgres://postgres:${databasePassword}@$(PROJECT_NAME)_${input.databaseServiceName}:5432/$(PROJECT_NAME)`,
+        `CMD_USECDN=false`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 3000,
+        },
+      ],
+      mounts: [
+        {
+          type: "volume",
+          name: "upload-data",
+          mountPath: "/home/hackmd/app/public/uploads",
+        },
+        {
+          type: "volume",
+          name: "database-data",
+          mountPath: "/var/lib/postgresql/data",
+        },
+      ],
+    },
+  });
+
+  services.push({
+    type: "postgres",
+    data: {
+      serviceName: input.databaseServiceName,
+      password: databasePassword,
+    },
+  });
+
+  return { services };
+}
