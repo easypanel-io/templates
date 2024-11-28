af42f8bffd67bd74b4fa29b2944775d3ed801537
serban-alexandru <alexserb2009@yahoo.com>
2024-11-28 11:03:18 +0200

    qbittorrent template

---
+++ templates/qbittorrent/index.ts
@@ -0,0 +1,44 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      env: [
+        `PUID=1000`,
+        `PGID=1000`,
+        `TZ=${input.serviceTimezone}`,
+        `WEBUI_PORT=${input.webUIPort}`,
+        `TORRENTING_PORT=${input.torrentingPort}`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: input.webUIPort,
+        },
+      ],
+      mounts: [
+        {
+          type: "volume",
+          name: "config",
+          mountPath: "/config",
+        },
+        {
+          type: "volume",
+          name: "downloads",
+          mountPath: "/downloads",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
