42b9bfc3357c1da4ac4026433bf48db52ee779d4
serban-alexandru <alexserb2009@yahoo.com>
2024-12-20 13:02:33 +0200

    classicpress template

---
+++ templates/classicpress/index.ts
@@ -0,0 +1,47 @@
+import { Output, randomPassword, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+  const databasePassword = randomPassword();
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      env: [
+        `CLASSICPRESS_DB_HOST=$(PROJECT_NAME)_${input.databaseServiceName}`,
+        `CLASSICPRESS_DB_USER=mariadb`,
+        `CLASSICPRESS_DB_PASSWORD=${databasePassword}`,
+        `CLASSICPRESS_DB_NAME=$(PROJECT_NAME)`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 80,
+        },
+      ],
+      mounts: [
+        {
+          type: "volume",
+          name: "data",
+          mountPath: "/var/www/html",
+        },
+      ],
+    },
+  });
+
+  services.push({
+    type: "mariadb",
+    data: {
+      serviceName: input.databaseServiceName,
+      password: databasePassword,
+    },
+  });
+
+  return { services };
+}
