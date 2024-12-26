410f6ffa1cb3cf492bee9fc92b5550c35c4e81bf
Ahson Shaikh <ahsonshaikh616@gmail.com>
2024-12-26 19:02:51 +0500

    Template Daapd Added

---
+++ templates/daapd/index.ts
@@ -0,0 +1,39 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      env: [`PUID=1000`, `PGID=1000`, `TZ=Etc/UTC`].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          path: "/",
+          port: 3689,
+        },
+      ],
+      mounts: [
+        {
+          type: "volume",
+          name: "music",
+          mountPath: "/music",
+        },
+        {
+          type: "volume",
+          name: "config",
+          mountPath: "/config",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
