c0b59028b96f5d4a17e4bb7f561c3d0020f4d26c
Ahson Shaikh <ahsonshaikh616@gmail.com>
2024-12-27 16:32:00 +0500

    Added Template Headphones

---
+++ templates/headphones/index.ts
@@ -0,0 +1,43 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      env: [`PUID=1000`, `PGID=1000`, `TZ=Etc/UTC`].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 8181,
+        },
+      ],
+      mounts: [
+        {
+          type: "volume",
+          name: "config",
+          mountPath: "/config",
+        },
+        {
+          type: "volume",
+          name: "downloads",
+          mountPath: "/downloads",
+        },
+        {
+          type: "volume",
+          name: "music",
+          mountPath: "/music",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
