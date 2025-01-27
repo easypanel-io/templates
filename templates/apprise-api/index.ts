8d8ce838f87f94cb4ccf9ab28257d922e410510e
Ahson Shaikh <ahsonshaikh616@gmail.com>
2025-01-27 19:13:21 +0500

    Added Template Apprise API

---
+++ templates/apprise-api/index.ts
@@ -0,0 +1,38 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      env: [
+        `SERVICE_FQDN_APPRISE_8000=`,
+        `PUID=1000`,
+        `PGID=1000`,
+        `TZ=UTC`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: `$(EASYPANEL_DOMAIN)`,
+          port: 8000,
+        },
+      ],
+      mounts: [
+        {
+          type: "volume",
+          name: "config",
+          mountPath: "/config",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
