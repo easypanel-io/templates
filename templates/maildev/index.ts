65481617338abc44dbf702d98f06db0a60d4d031
Ahson Shaikh <ahsonshaikh616@gmail.com>
2025-02-03 18:18:43 +0500

    Added Template MailDev

---
+++ templates/maildev/index.ts
@@ -0,0 +1,35 @@
+import { Output, Services, randomPassword } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+  const databasePassword = randomPassword();
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      env: [
+        `TZ=Asia/Shanghai`,
+        `MAILDEV_WEB_PORT=1080`,
+        `MAILDEV_SMTP_PORT=1025`,
+      ].join("\n"),
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 1080,
+        },
+        {
+          host: "smtp-$(EASYPANEL_DOMAIN)",
+          port: 1025,
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
