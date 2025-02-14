9b42ab299315f6a78d6c02c0460e34e69d18dd89
ShayPunter <shay@shaypunter.co.uk>
2025-02-14 12:07:52 +0100

    Akaunting, change to managed mysql process, change easy panel domain to primary domain, allowed user to enter admin pass

--- templates/akaunting/index.ts
+++ templates/akaunting/index.ts
@@ -5,28 +5,12 @@ export function generate(input: Input): Output {
   const services: Services = [];
   const dbPassword = randomPassword();
 
-  // Database service
+  // Database service - using managed MySQL
   services.push({
-    type: "app",
+    type: "mysql",
     data: {
       serviceName: `${input.appServiceName}-db`,
-      source: {
-        type: "image",
-        image: "mariadb:11.4.5",
-      },
-      env: [
-        `MARIADB_ROOT_PASSWORD=${dbPassword}`,
-        `MARIADB_DATABASE=${input.dbName}`,
-        `MARIADB_USER=${input.dbUser}`,
-        `MARIADB_PASSWORD=${dbPassword}`,
-      ].join("\n"),
-      mounts: [
-        {
-          type: "volume",
-          name: "db",
-          mountPath: "/var/lib/mysql",
-        },
-      ],
+      password: dbPassword,
     },
   });
 
@@ -41,21 +25,21 @@ export function generate(input: Input): Output {
       },
       env: [
         `AKAUNTING_SETUP=false`,
-        `APP_URL=https://$(EASYPANEL_DOMAIN)`,
+        `APP_URL=$(PRIMARY_DOMAIN)`,
         `APP_DEBUG=false`,
-        `DB_HOST=$(PROJECT_NAME)_${input.appServiceName}-db`,
+        `DB_HOST=${input.appServiceName}-db`,
         `DB_PORT=3306`,
-        `DB_DATABASE=${input.dbName}`,
-        `DB_USERNAME=${input.dbUser}`,
+        `DB_DATABASE=easypanel`,
+        `DB_USERNAME=easypanel`,
         `DB_PASSWORD=${dbPassword}`,
         `COMPANY_NAME=${input.companyName}`,
         `COMPANY_EMAIL=${input.companyEmail}`,
         `ADMIN_EMAIL=${input.adminEmail}`,
-        `ADMIN_PASSWORD=${randomPassword()}`,
+        `ADMIN_PASSWORD=${input.adminPassword}`,
       ].join("\n"),
       domains: [
         {
-          host: "$(EASYPANEL_DOMAIN)",
+          host: "$(PRIMARY_DOMAIN)",
           port: 80,
         },
       ],
