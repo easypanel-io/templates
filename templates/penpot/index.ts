e20912fb858908997c5ee4f77b002a2139e923c2
Ahson Shaikh <ahsonshaikh616@gmail.com>
2025-03-01 02:17:56 +0500

    Updated Templates

--- templates/penpot/index.ts
+++ templates/penpot/index.ts
@@ -21,7 +21,7 @@ export function generate(input: Input): Output {
       domains: [
         {
           host: "$(EASYPANEL_DOMAIN)",
-          port: 80,
+          port: 8080,
         },
       ],
       mounts: [
