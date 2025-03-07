ed90ce7eb1fb48677e367d533b7569b27094629b
Eloi Marques da Silva <eloimarquessilva@gmail.com>
2025-03-07 14:51:38 -0300

    Update index.ts
    services.push({
      type: "postgres",
      data: {
        serviceName: input.databaseServiceName,
        image: "postgres:12",
        password: randomPasswordPostgres,
        deploy: {
          command: "docker-entrypoint.sh postgres && apt-get update && apt-get install -y postgresql-server-dev-12 git build-essential && git clone --branch v0.5.0 https://github.com/pgvector/pgvector.git && cd pgvector && make && make install && psql -U postgres -c \"CREATE EXTENSION vector;\""
        }
      },
    });

--- templates/chatwoot/index.ts
+++ templates/chatwoot/index.ts
@@ -103,13 +103,10 @@ services.push({
     image: "postgres:12",
     password: randomPasswordPostgres,
     deploy: {
-      command: [
-        "docker-entrypoint.sh postgres && apt-get update && apt-get install -y postgresql-server-dev-12 git build-essential && git clone --branch v0.5.0 https://github.com/pgvector/pgvector.git && cd pgvector && make && make install && psql -U postgres -c \"CREATE EXTENSION vector;\""
-      ]
+      command: "docker-entrypoint.sh postgres && apt-get update && apt-get install -y postgresql-server-dev-12 git build-essential && git clone --branch v0.5.0 https://github.com/pgvector/pgvector.git && cd pgvector && make && make install && psql -U postgres -c \"CREATE EXTENSION vector;\""
     }
   },
 });
-
 
   return { services };
 }
