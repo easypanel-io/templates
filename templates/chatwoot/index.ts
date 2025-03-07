5c9ea25bdc798b664ef59d2ded72a36c6c19c09e
Eloi Marques da Silva <eloimarquessilva@gmail.com>
2025-03-07 14:44:49 -0300

    Update index.ts
    This commit includes updates to PostgreSQL 12 to ensure compatibility with Chatwoot version 4 or higher. It involves the installation of required dependencies and the pgvector extension.
    
    Changes:
    Install necessary packages (postgresql-server-dev-12, git, build-essential).
    
    Clone, compile, and install the pgvector extension.
    
    Create the pgvector extension within the PostgreSQL database to enhance search and vector storage capabilities.
    
    These changes will ensure PostgreSQL is properly configured for running Chatwoot v4+.

--- templates/chatwoot/index.ts
+++ templates/chatwoot/index.ts
@@ -96,14 +96,20 @@ export function generate(input: Input): Output {
     },
   });
 
-  services.push({
-    type: "postgres",
-    data: {
-      serviceName: input.databaseServiceName,
-      image: "postgres:12",
-      password: randomPasswordPostgres,
-    },
-  });
+services.push({
+  type: "postgres",
+  data: {
+    serviceName: input.databaseServiceName,
+    image: "postgres:12",
+    password: randomPasswordPostgres,
+    deploy: {
+      command: [
+        "docker-entrypoint.sh postgres && apt-get update && apt-get install -y postgresql-server-dev-12 git build-essential && git clone --branch v0.5.0 https://github.com/pgvector/pgvector.git && cd pgvector && make && make install && psql -U postgres -c \"CREATE EXTENSION vector;\""
+      ]
+    }
+  },
+});
+
 
   return { services };
 }
