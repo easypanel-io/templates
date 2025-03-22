b5bbad95c059c0e3c9db668664f5ea98a8c43dcb
Alan Mosko <moskoweb@gmail.com>
2025-03-22 09:27:02 -0300

    wip

--- templates/n8n/index.ts
+++ templates/n8n/index.ts
@@ -9,39 +9,43 @@ import { Input } from "./meta";
 export function generate(input: Input): Output {
   const services: Services = [];
   const encryptionKey = randomString(32);
-  const databasePassword = randomPassword();
-  const redisPassword = randomPassword();
   const envs = [
     `N8N_HOST=$(PRIMARY_DOMAIN)`,
     `WEBHOOK_URL=https://$(PRIMARY_DOMAIN)`,
     `N8N_PROTOCOL=https`,
-    `GENERIC_TIMEZONE=UTC`,
+    `GENERIC_TIMEZONE=${input.appTimezone}`,
     `N8N_ENCRYPTION_KEY=${encryptionKey}`,
     `N8N_ENFORCE_SETTINGS_FILE_PERMISSIONS=true`,
     'N8N_RUNNERS_ENABLED=true'
   ];
   
   if (input.redisService) {
+    const redisServiceName = input.appServiceName + '-redis';
+    const redisPassword = randomPassword();
+    
     services.push({
       type: "redis",
       data: {
-        serviceName: input.appServiceName + '-redis',
+        serviceName: redisServiceName,
         password: redisPassword
       },
     });
 
     envs.push('// Redis');
-    envs.push(`QUEUE_BULL_REDIS_HOST=$(PROJECT_NAME)_${input.redisServiceName}`,);
+    envs.push(`QUEUE_BULL_REDIS_HOST=$(PROJECT_NAME)_${redisServiceName}`,);
     envs.push(`QUEUE_BULL_REDIS_USERNAME=default`,);
     envs.push(`QUEUE_BULL_REDIS_PASSWORD=${redisPassword}`,);
     envs.push(`QUEUE_BULL_PREFIX=n8n`,);
   }
 
   if (input.databaseType === 'postgres') {
+    const databaseServiceName = input.appServiceName + '-db';
+    const databasePassword = randomPassword();
+
     services.push({
       type: "postgres",
       data: {
-        serviceName: input.appServiceName + '-db',
+        serviceName: databaseServiceName,
         password: databasePassword,
       },
     });
@@ -49,7 +53,7 @@ export function generate(input: Input): Output {
     envs.push('// Database');
     envs.push(`DB_TYPE=postgresdb`);
     envs.push(`DB_POSTGRESDB_DATABASE=$(PROJECT_NAME)`);
-    envs.push(`DB_POSTGRESDB_HOST=$(PROJECT_NAME)_${input.databaseServiceName}`);
+    envs.push(`DB_POSTGRESDB_HOST=$(PROJECT_NAME)_${databaseServiceName}`);
     envs.push(`DB_POSTGRESDB_PORT=5432`);
     envs.push(`DB_POSTGRESDB_USER=postgres`);
     envs.push(`DB_POSTGRESDB_PASSWORD=${databasePassword}`);
