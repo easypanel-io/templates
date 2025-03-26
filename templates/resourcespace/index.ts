027e1b1743e47ba86a70fbe05a83a831648d7a49
Ahson Shaikh <ahsonshaikh616@gmail.com>
2025-03-26 22:17:16 +0500

    Added Template Resource Space

---
+++ templates/resourcespace/index.ts
@@ -0,0 +1,51 @@
+import { Output, Services, randomPassword } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+  const databasePassword = randomPassword();
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      mounts: [
+        {
+          type: "volume",
+          name: "include",
+          mountPath: "/var/www/html/include",
+        },
+        {
+          type: "volume",
+          name: "filestore",
+          mountPath: "/var/www/html/filestore",
+        },
+        {
+          type: "volume",
+          name: "storage",
+          mountPath: "/srv/storage/testdir",
+        },
+      ],
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 80,
+        },
+      ],
+    },
+  });
+
+  services.push({
+    type: "mysql",
+    data: {
+      serviceName: `${input.appServiceName}-db`,
+      password: databasePassword,
+    },
+  });
+
+  return { services };
+}
