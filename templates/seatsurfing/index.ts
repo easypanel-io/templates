e5e2fa6fb2a4cfb55e0809dab2ba3c37c3744804
Ahson Shaikh <ahsonshaikh616@gmail.com>
2025-04-07 19:33:46 +0500

    Added Temnplate Seatsurfing

---
+++ templates/seatsurfing/index.ts
@@ -0,0 +1,70 @@
+import {
+  Output,
+  Services,
+  randomPassword,
+  randomString,
+} from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+  const databasePassword = randomPassword();
+  const jwtSecretKey = randomString(32);
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 8080,
+        },
+      ],
+      env: [
+        `POSTGRES_URL=postgres://postgres:${databasePassword}@$(PROJECT_NAME)_${input.appServiceName}-db/$(PROJECT_NAME)?sslmode=disable`,
+        `JWT_SIGNING_KEY=${jwtSecretKey}`,
+        `BOOKING_UI_BACKEND=$(PROJECT_NAME)_${input.appServiceName}-booking:3001`,
+        `ADMIN_UI_BACKEND=$(PROJECT_NAME)_${input.appServiceName}-admin:3000`,
+        `PUBLIC_URL=https://$(PRIMARY_DOMAIN)`,
+        `FRONTEND_URL=https://$(PRIMARY_DOMAIN)`,
+      ].join("\n"),
+    },
+  });
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: `${input.appServiceName}-booking`,
+      source: {
+        type: "image",
+        image: "seatsurfing/booking-ui",
+      },
+    },
+  });
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: `${input.appServiceName}-admin`,
+      source: {
+        type: "image",
+        image: "seatsurfing/admin-ui",
+      },
+    },
+  });
+
+  services.push({
+    type: "postgres",
+    data: {
+      serviceName: `${input.appServiceName}-db`,
+      password: databasePassword,
+    },
+  });
+
+  return { services };
+}
