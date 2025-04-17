49b25f73597622c1577fc022baf88b64a3705d99
Ahson Shaikh <ahsonshaikh616@gmail.com>
2025-04-17 19:57:45 +0500

    Added Template Honey

---
+++ templates/honey/index.ts
@@ -0,0 +1,32 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 4173,
+        },
+      ],
+      mounts: [
+        {
+          type: "volume",
+          name: "config",
+          mountPath: "/app/dist/config",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
