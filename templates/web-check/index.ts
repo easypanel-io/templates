f279146550fb555c49af31b13c0fd07d0840abbd
Ahson Shaikh <ahsonshaikh@Ahsons-MacBook-Air.local>
2025-05-12 15:43:20 +0500

    Added Template WebCheck

---
+++ templates/web-check/index.ts
@@ -0,0 +1,26 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 3000,
+        },
+      ],
+    },
+  });
+
+  return { services };
+} 
