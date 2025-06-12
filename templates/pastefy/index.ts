8d2bf9bc6a7e2626d69d99fc69cf221c34ec62f8
Ahson Shaikh <ahsonshaikh616@gmail.com>
2025-06-12 19:18:53 +0500

    Added Template Pastefy

---
+++ templates/pastefy/index.ts
@@ -0,0 +1,45 @@
+import { Output, randomPassword, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+  const databasePassword = randomPassword();
+
+  services.push({
+    type: "mariadb",
+    data: {
+      serviceName: `${input.appServiceName}-db`,
+      password: databasePassword,
+    },
+  });
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      env: [
+        `HTTP_SERVER_PORT=80`,
+        `HTTP_SERVER_CORS=*`,
+        `DATABASE_DRIVER=mysql`,
+        `DATABASE_NAME=$(PROJECT_NAME)`,
+        `DATABASE_USER=mariadb`,
+        `DATABASE_PASSWORD=${databasePassword}`,
+        `DATABASE_HOST=$(PROJECT_NAME)_${input.appServiceName}-db`,
+        `DATABASE_PORT=3306`,
+        `SERVER_NAME=${input.serverName}`,
+      ].join("\n"),
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 80,
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
