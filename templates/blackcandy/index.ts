d21e5da28221d804579bc974cfce5d1ef3e49915
Ahson Shaikh <ahsonshaikh616@gmail.com>
2025-06-27 05:39:25 +0500

    Added Template Black Candy

---
+++ templates/blackcandy/index.ts
@@ -0,0 +1,33 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 3000,
+        },
+      ],
+      env: [`MEDIA_PATH=/media_data`].join("\n"),
+      mounts: [
+        {
+          type: "bind",
+          hostPath: `${input.mediaPath}`,
+          mountPath: "/media_data",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
