50a2052c05f71ab7fe6c44b7a4216dfcb550887a
Ahson Shaikh <ahsonshaikh616@gmail.com>
2025-09-10 15:08:00 +0500

    Added Template Memgraph

---
+++ templates/memgraph/index.ts
@@ -0,0 +1,46 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: `${input.appServiceName}-db`,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 7687,
+        },
+      ],
+    },
+  });
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.labImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 3000,
+        },
+      ],
+      env: [
+        `QUICK_CONNECT_MG_HOST=$(PROJECT_NAME)_${input.appServiceName}-db`,
+        "QUICK_CONNECT_MG_PORT=7687",
+      ].join("\n"),
+    },
+  });
+
+  return { services };
+}
