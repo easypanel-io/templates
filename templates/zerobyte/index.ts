13835b5cab6803c93f8266f4207a6ed39bc8898c
Ahson Shaikh <ahsonshaikh616@gmail.com>
2026-02-23 02:33:35 +0500

    Revert "Updated 182 Files out of 326 Templates"
    This reverts commit e099a8002050dcabccf84aaaf50b3c1751ec714f.

--- templates/zerobyte/index.ts
+++ templates/zerobyte/index.ts
@@ -1,8 +1,15 @@
-import { Output, Services } from "~templates-utils";
+import { Output, randomString, Services } from "~templates-utils";
 import { Input } from "./meta";
 
 export function generate(input: Input): Output {
   const services: Services = [];
+  const appSecret = input.appSecret || randomString(64);
+
+  const env = [
+    `TZ=${input.timezone || "UTC"}`,
+    `APP_SECRET=${appSecret}`,
+    `BASE_URL=${input.baseUrl || "https://$(PRIMARY_DOMAIN)"}`,
+  ];
 
   services.push({
     type: "app",
@@ -18,7 +25,7 @@ export function generate(input: Input): Output {
           port: 4096,
         },
       ],
-      env: [`TZ=${input.timezone || "UTC"}`].join("\n"),
+      env: env.join("\n"),
       mounts: [
         {
           type: "volume",
