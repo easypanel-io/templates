bfb213a502ae7637a9d1db7d1f95777f7c6fb294
Ahson Shaikh <ahsonshaikh616@gmail.com>
2026-03-20 03:30:17 +0500

    Updated & Tested

--- templates/kan/index.ts
+++ templates/kan/index.ts
@@ -1,21 +1,30 @@
-import {
-  Output,
-  randomPassword,
-  randomString,
-  Services,
-} from "~templates-utils";
+import { randomBytes } from "crypto";
+import { Output, randomPassword, Services } from "~templates-utils";
 import { Input } from "./meta";
 
 export function generate(input: Input): Output {
   const services: Services = [];
   const databasePassword = randomPassword();
-  const authSecret = randomString(32);
+  const authSecret = randomBytes(32).toString("base64");
+  const postgresUrl = `postgresql://postgres:${databasePassword}@$(PROJECT_NAME)_${input.appServiceName}-db:5432/$(PROJECT_NAME)`;
 
   services.push({
     type: "postgres",
     data: {
       serviceName: `${input.appServiceName}-db`,
       password: databasePassword,
+    },
+  });
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: `${input.appServiceName}-migrate`,
+      source: {
+        type: "image",
+        image: input.migrateServiceImage,
+      },
+      env: [`POSTGRES_URL=${postgresUrl}`].join("\n"),
     },
   });
 
@@ -36,7 +45,7 @@ export function generate(input: Input): Output {
       env: [
         `NEXT_PUBLIC_BASE_URL=https://$(PRIMARY_DOMAIN)`,
         `BETTER_AUTH_SECRET=${authSecret}`,
-        `POSTGRES_URL=postgresql://postgres:${databasePassword}@$(PROJECT_NAME)_${input.appServiceName}-db:5432/$(PROJECT_NAME)`,
+        `POSTGRES_URL=${postgresUrl}`,
         `NEXT_PUBLIC_ALLOW_CREDENTIALS=true`,
       ].join("\n"),
     },
