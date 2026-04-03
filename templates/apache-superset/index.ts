de88f9a9069cddf866f52cc1c2745cf9bc649e86
Ahson Shaikh <ahsonshaikh616@gmail.com>
2026-04-03 17:31:06 +0500

    Updated

--- templates/apache-superset/index.ts
+++ templates/apache-superset/index.ts
@@ -1,10 +1,8 @@
-import { Output, Services, randomPassword, randomString } from "~templates-utils";
+import { Output, Services, randomString } from "~templates-utils";
 import { Input } from "./meta";
 
 export function generate(input: Input): Output {
   const services: Services = [];
-  const dbPassword = randomPassword();
-  const redisPassword = randomPassword();
   const secretKey = randomString(32);
 
   services.push({
@@ -23,14 +21,6 @@ export function generate(input: Input): Output {
       ],
       env: [
         `SUPERSET_SECRET_KEY=${secretKey}`,
-        `POSTGRES_DB=$(PROJECT_NAME)`,
-        `POSTGRES_USER=postgres`,
-        `POSTGRES_PASSWORD=${dbPassword}`,
-        `POSTGRES_HOST=$(PROJECT_NAME)_${input.appServiceName}-db`,
-        `POSTGRES_PORT=5432`,
-        `REDIS_HOST=$(PROJECT_NAME)_${input.appServiceName}-redis`,
-        `REDIS_PORT=6379`,
-        `REDIS_PASSWORD=${redisPassword}`,
         `SUPERSET_LOAD_EXAMPLES=yes`,
         `SUPERSET_ENABLE_PROXY_FIX=true`,
       ].join("\n"),
@@ -44,21 +34,5 @@ export function generate(input: Input): Output {
     },
   });
 
-  services.push({
-    type: "postgres",
-    data: {
-      serviceName: `${input.appServiceName}-db`,
-      password: dbPassword,
-    },
-  });
-
-  services.push({
-    type: "redis",
-    data: {
-      serviceName: `${input.appServiceName}-redis`,
-      password: redisPassword,
-    },
-  });
-
   return { services };
-} 
+}
