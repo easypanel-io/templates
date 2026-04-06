f3f538c42d07da0bfb5728fe1953eeb290cafc5c
Ahson Shaikh <ahsonshaikh616@gmail.com>
2026-04-06 21:15:25 +0500

    Added Template Vane

---
+++ templates/vane/index.ts
@@ -0,0 +1,40 @@
+import { Output, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+
+  const appPort = input.appPort || "3000";
+  const env: string[] = [];
+
+  if (input.searxngApiUrl) {
+    env.push(`SEARXNG_API_URL=${input.searxngApiUrl}`);
+  }
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      env: env.join("\n"),
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: Number(appPort),
+        },
+      ],
+      mounts: [
+        {
+          type: "volume",
+          name: "data",
+          mountPath: "/home/vane/data",
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
