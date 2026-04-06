9d8cd2f2d04d2a7581b3736d7e141c11ec03eb12
Ahson Shaikh <ahsonshaikh616@gmail.com>
2026-04-07 02:41:49 +0500

    Added Template Ocular

---
+++ templates/ocular/index.ts
@@ -0,0 +1,45 @@
+import { Output, randomPassword, randomString, Services } from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+  const jwtSecret = randomString(32);
+  const tokenExpiration = input.genesisJwtTokenExpiration ?? 120000;
+  const cookieAllowHttp = (input.genesisJwtCookieAllowHttp ?? false)
+    ? "true"
+    : "false";
+  const createUsers =
+    input.genesisCreateUsers?.trim() || `admin!:${randomPassword()}`;
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      env: [
+        `GENESIS_JWT_SECRET=${jwtSecret}`,
+        `GENESIS_JWT_TOKEN_EXPIRATION=${tokenExpiration}`,
+        `GENESIS_CREATE_USERS=${createUsers}`,
+        `GENESIS_JWT_COOKIE_ALLOW_HTTP=${cookieAllowHttp}`,
+      ].join("\n"),
+      mounts: [
+        {
+          type: "volume",
+          name: "ocular-genesis-data",
+          mountPath: "/data/genesis",
+        },
+      ],
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 80,
+        },
+      ],
+    },
+  });
+
+  return { services };
+}
