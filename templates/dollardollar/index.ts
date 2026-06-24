5e98290d2f4e7fec6516397b7b9270b7937ed178
Ahson Shaikh <ahsonshaikh616@gmail.com>
2026-06-24 22:22:15 +0500

    Added Template Dollar Dollar

---
+++ templates/dollardollar/index.ts
@@ -0,0 +1,56 @@
+import {
+  Output,
+  randomPassword,
+  randomString,
+  Services,
+} from "~templates-utils";
+import { Input } from "./meta";
+
+export function generate(input: Input): Output {
+  const services: Services = [];
+  const databasePassword = randomPassword();
+  const secretKey = randomString(32);
+
+  services.push({
+    type: "app",
+    data: {
+      serviceName: input.appServiceName,
+      source: {
+        type: "image",
+        image: input.appServiceImage,
+      },
+      domains: [
+        {
+          host: "$(EASYPANEL_DOMAIN)",
+          port: 5001,
+        },
+      ],
+      env: [
+        `SQLALCHEMY_DATABASE_URI=postgresql://postgres:${databasePassword}@$(PROJECT_NAME)-${input.appServiceName}-db:5432/$(PROJECT_NAME)`,
+        `SECRET_KEY=${secretKey}`,
+        `FLASK_APP=app.py`,
+        `DEVELOPMENT_MODE=False`,
+        `DEBUG=False`,
+        `LOG_LEVEL=INFO`,
+        `DISABLE_SIGNUPS=False`,
+      ].join("\n"),
+      mounts: [
+        {
+          type: "volume",
+          name: "data",
+          mountPath: "/app/data",
+        },
+      ],
+    },
+  });
+
+  services.push({
+    type: "postgres",
+    data: {
+      serviceName: `${input.appServiceName}-db`,
+      password: databasePassword,
+    },
+  });
+
+  return { services };
+}
